6ea36117e70a1faa7799c5ff1c65a1c59d2ddb20
Paul Asmuth <paul@paulasmuth.com>
2011-10-07 15:58:06 +0200

    graph_widget: labels

--- lib/fnordmetric/count_metric.rb
+++ lib/fnordmetric/count_metric.rb
@@ -1,2 +1,7 @@
 class FnordMetric::CountMetric < FnordMetric::Metric
+
+  def value_at
+    42 #fixme
+  end
+
 end
--- lib/fnordmetric/graph_widget.rb
+++ lib/fnordmetric/graph_widget.rb
@@ -2,19 +2,23 @@ class FnordMetric::GraphWidget < FnordMetric::Widget
 
   def data
     super.merge(
-      :x_labels => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],      
-      :series => metrics.map{|m| build_series(m) },
+      :x_labels => ticks.map{ |t| build_label(t) },      
+      :series => metrics.map{ |m| build_series(m) },
       :widget_url => "/widget_graph.js?#{(rand*999).to_i}"
     )
   end
 
 private
 
+  def build_label(tick)
+    tick.first.strftime("%d.%m")
+  end
+
   def build_series(metric)
     { 
       :name => metric.token,
       :data => ticks.map{ |tick| metric.at(tick.last) }
-    )
+    }
   end
 
 end
