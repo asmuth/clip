51cb32ebd91420f54815446a629014d2c798a63a
Paul Asmuth <paul@23linesofcode.com>
2011-10-09 19:09:29 +0200

    move it!

--- pub/fnordmetric/widget_timeline.js
+++ pub/fnordmetric/widget_timeline.js
@@ -3,22 +3,9 @@ FnordMetric.js('highcharts/highcharts.js', function(){
 FnordMetric.css('fnordmetric.css', function(){});
 FnordMetric.css('widget_timeline.css', function(){});
 
-  $('body').append( $('<div></div>').attr('class', 'headbar').append(
-    $('<div></div>').attr('class', 'datepicker')
-  ).append(
-    $('<div></div>').attr('class', 'button').append($('<span></span>').html('Foobar'))
-  ).append(
-    $('<div></div>').attr('class', 'button').append($('<span></span>').html('Barfoo'))
-  ).append(
-    $('<h2></h2>').html(widget_config.title)
-  ) );
-  $('body').append( $('<div></div>').attr('id', 'container') );
 
-
-  console.log(widget_config);
-
-
-  var chart_options = {     
+  drawLayout();
+  chart = new Highcharts.Chart({     
     chart: { renderTo: 'container', defaultSeriesType: widget_config.chart_type, height: 270 },
     series: [],
     title: { text: '' },
@@ -41,24 +28,70 @@ FnordMetric.css('widget_timeline.css', function(){});
       margin: 25,
       borderWidth: 0
     },
-  };
+  });
+  redrawWithRange(true);
 
-
-  chart = new Highcharts.Chart(chart_options);
-
-  for(n in widget_config.metrics){
+  function redrawWithRange(first_time){
+    redrawDatepicker();
     var _query = '?at='+widget_config.start_timestamp+'-'+widget_config.end_timestamp+
-                 '&tick='+widget_config.tick+(widget_config.delta ? '&delta=1' : '');
-    $.ajax({
-      url: '/fnordmetric/metric/'+widget_config.metrics[n]+_query, 
-      success: function(json){                
-        for(i in json.values){ json.values[i][0] = json.values[i][0]*1000; }
-        chart.addSeries({name: widget_config.metrics[n], data: json.values}); 
-        chart.redraw();
-      }
-    });         
-  }  
+                 '&tick='+widget_config.tick+(widget_config.delta ? '&delta=1' : '');    
+    chart.series = [];
+    for(n in widget_config.metrics){
+      $.ajax({
+        url: '/fnordmetric/metric/'+widget_config.metrics[n]+_query, 
+        success: redrawMetric(first_time, n)
+      });         
+    }  
+  }
 
+  function redrawMetric(first_time, n){
+    return (function(json){                   
+      for(i in json.values){ json.values[i][0] = json.values[i][0]*1000; }
+      if(!first_time){ 
+        chart.get('series-'+n).setData(json.values);
+      } else {
+        chart.addSeries({name: widget_config.metrics[n], data: json.values, id: 'series-'+n });     
+      }
+    });
+  }
+
+  function redrawDatepicker(){
+    $('.datepicker').html(
+      Highcharts.dateFormat('%d.%m.%y %H:%M', parseInt(widget_config.start_timestamp)*1000) + 
+      '&nbsp;&dash;&nbsp;' +
+      Highcharts.dateFormat('%d.%m.%y %H:%M', parseInt(widget_config.end_timestamp)*1000) 
+    );
+  }
+
+  function moveRange(direction){
+    v = widget_config.tick*direction*8;
+    widget_config.start_timestamp += v;
+    widget_config.end_timestamp += v;
+    redrawWithRange();
+  }
+  
+  function drawLayout(){
+    $('body').append( $('<div></div>').attr('class', 'headbar').append(
+      $('<div></div>').attr('class', 'button mr').append($('<span></span>').html('delta'))
+    ).append(
+      $('<div></div>').attr('class', 'button').append($('<span></span>').html('1h'))
+    ).append(
+      $('<div></div>').attr('class', 'button').append($('<span></span>').html('1d'))
+    ).append(
+      $('<div></div>').attr('class', 'button mr').append($('<span></span>').html('&rarr;')).click(
+        function(){ moveRange(1); }
+      )
+    ).append(
+      $('<div></div>').attr('class', 'datepicker')
+    ).append(
+      $('<div></div>').attr('class', 'button').append($('<span></span>').html('&larr;')).click(
+        function(){ moveRange(-1); }
+      )
+    ).append(
+      $('<h2></h2>').html(widget_config.title)
+    ) );
+    $('body').append( $('<div></div>').attr('id', 'container') );
+  }
 
 });  
 });
