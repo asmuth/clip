8c7c057b454916486a7774965437b69fbbfce5cb
Paul Asmuth <paul@paulasmuth.com>
2011-10-10 13:43:13 +0200

    numbers widget: autoupdate

--- pub/fnordmetric/widget_numbers.js
+++ pub/fnordmetric/widget_numbers.js
@@ -2,21 +2,36 @@ FnordMetric.css('fnordmetric.css', function(){});
 FnordMetric.js('jquery-1.6.1.min.js', function(){  
 
   drawLayout();
-  updateValues(4);
+  updateDataAndValues();
+
+  window.setInterval(updateDataAndValues, 3000);
 
   function drawLayout(){
     for(n in widget_config.metrics){
-      $('body').append(container=$('<div></div>').attr('class', 'numbers_container'));
+      $('body').append(container=$('<div></div>').attr('class', 'numbers_container').attr('rel', widget_config.metrics[n]));
       container.append($('<div></div>').addClass('title').html(widget_config.metrics[n]));
       for(var k in widget_config.intervals){
         var u = "/fnordmetric/metric/"+widget_config.metrics[n]+'?'+widget_config.intervals[k];
-        container.append($('<div></div>').addClass('number').attr('rel', u).attr('data',123).append(
+        container.append($('<div></div>').addClass('number').attr('rel', u).attr('data',0).append(
           $('<span></span>').addClass('value').html(0)
         ).append(
           $('<span></span>').addClass('desc').html(k)
         ));
       } 
     }
+  }
+
+  function updateDataAndValues(){
+    for(n in widget_config.metrics){
+      $.get('/fnordmetric/metric/'+widget_config.metrics[n], updateSingleNumber(widget_config.metrics[n]));
+    }
+  }
+
+  function updateSingleNumber(metric_name){
+    return function(json){
+      $('.number:first', $('.numbers_container[rel="'+metric_name+'"]')).attr('data', json.value);
+      updateValues(4);
+    };
   }
 
   function updateValues(diff_factor){
@@ -26,15 +41,13 @@ FnordMetric.js('jquery-1.6.1.min.js', function(){
       var current_val = parseFloat($('.value', this).html());
       var diff = (target_val-current_val)/diff_factor; 
       if(diff < 1){ diff=1; }
-      console.log(diff);
-      if(target_val > current_val-diff){ 
+      if(target_val > current_val){ 
         still_running = true;
         var new_val = current_val+diff;
         if(new_val > target_val){ new_val = target_val; }
         $('.value', this).html(new_val.toFixed(widget_config.round_to));
       }
     });
-    console.log(still_running);
     if(still_running){ 
       (function(df){
         window.setTimeout(function(){ updateValues(df); }, 30);
