1444d85d4e5790d5037a7fba381c42fdada91647
Paul Asmuth <paul@23linesofcode.com>
2011-11-13 14:08:20 +0100

    don't parse json in inbound stream

--- lib/fnordmetric/inbound_stream.rb
+++ lib/fnordmetric/inbound_stream.rb
@@ -1,39 +1,55 @@
-class FnordMetric::InboundStream  < EventMachine::Connection 
+class FnordMetric::InboundStream < EventMachine::Connection 
 
   def post_init
      @redis = EM::Hiredis.connect("redis://localhost:6379")
-     @parser = Yajl::Parser.new
-     @parser.on_parse_complete = method(:process_event)
+     @streaming = true
      @buffer = ""
+     @events = []
   end
 
-  def unbind                
-    @redis.close_connection
-    @finito = true    
+  def get_next_uuid
+    rand(9999999999999999999).to_s
+  end
+  
+  def unbind
+    @streaming = false
   end
 
-  def process_event(event)       
-    event_data = Yajl::Encoder.encode(event)
-    EM.defer{ push_event(event_data) }
+  def receive_data(chunk)        
+    @buffer << chunk         
+    EM.defer{ read_next_event }
+  end
+  
+  def read_next_event
+    while (event = @buffer.slice!(/^(.*)\n/))
+      @events << event
+    end 
+    EM.next_tick(&method(:push_next_event))
   end
 
-  def push_event(event_data)    
-    my_uuid = rand(9999999999999999999).to_s # FIXME 
-    @redis.set("fnordmetric-event-#{my_uuid}", event_data).callback do
-      @redis.lpush("fnordmetric-queue", my_uuid).callback do
-        # events that aren't processed within 60 seconds get dropped
-        @redis.expire("fnordmetric-event-#{my_uuid}", 60).callback do
-          @redis.hincrby("fnordmetric-stats", "events_received", 1) do
-            # HACK!!! - otherwise the connection is never closed :( 
-            @redis.instance_variable_get(:@connection).close_connection if @finito == true
+  def push_next_event
+    return hacky_close_connection? if @events.empty?
+    push_event(get_next_uuid, @events.pop)    
+  end
+
+  def push_event(event_id, event_data)
+    @redis.hincrby("fnordmetric-stats", "events_received", 1) do
+      @redis.set("fnordmetric-event-#{event_id}", event_data) do
+        @redis.lpush("fnordmetric-queue", event_id) do
+          @redis.expire("fnordmetric-event-#{event_id}", 60) do
+            EM.next_tick(&method(:push_next_event))     
           end
         end
       end
     end
   end
 
-  def receive_data(chunk)        
-    @parser << chunk         
+  def hacky_close_connection?
+    return true if @streaming
+    @redis.instance_variable_get(:@connection).tap do |c|
+      # this is uber-hacky but it works...
+      c.close_connection
+    end
   end
 
 end
