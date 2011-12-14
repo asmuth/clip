b8720e119939d9b7261d7bbe34f5cc3e1f64270f
Paul Asmuth <paul@paulasmuth.com>
2011-12-14 11:24:54 +0100

    back2highcharts (1/n)

--- pub/fnordmetric.js
+++ pub/fnordmetric.js
@@ -44,10 +44,117 @@ var FnordMetric = (function(){
   };
 
   var timelineWidget = function(opts){
+  
+    var chart=false;
+
+    opts.elem_inner = $('.inner', opts.elem);
+    //redrawWithRange(true);
+
+    //if(widget_config.autoupdate){
+    //  window.setInterval(function(){
+    //    redrawWithRange(false, true);
+    //  }, 3000);
+    // }
+
+    function redrawWithRange(first_time, silent){
+      if(!silent){ $("#container").css('opacity', 0.5); }
+      redrawDatepicker();    
+      var _query = '?at='+widget_config.start_timestamp+'-'+widget_config.end_timestamp+
+                   '&tick='+widget_config.tick+(widget_config.delta ? '&delta=1' : '');    
+      chart.series = [];
+      metrics_completed = 0;
+      for(n in widget_config.metrics){
+        $.ajax({
+          url: FnordMetric.p+'/metric/'+widget_config.metrics[n]+_query, 
+          success: redrawMetric(first_time, n)
+        });         
+      }  
+    }
+
+    function redrawMetric(first_time, n){
+      return (function(json){                   
+        for(i in json.values){ json.values[i][0] = json.values[i][0]*1000; }
+        if(!first_time){ 
+          chart.get('series-'+n).setData(json.values);
+        } else {
+          chart.addSeries({name: widget_config.metrics[n], data: json.values, id: 'series-'+n });     
+        }       
+        if((metrics_completed += 1) == widget_config.metrics.length){ 
+          $("#container").css('opacity', 1);
+        }
+      });
+    }
+
+    function redrawDatepicker(){
+      $('.datepicker').html(
+        Highcharts.dateFormat('%d.%m.%y %H:%M', parseInt(widget_config.start_timestamp)*1000) + 
+        '&nbsp;&dash;&nbsp;' +
+        Highcharts.dateFormat('%d.%m.%y %H:%M', parseInt(widget_config.end_timestamp)*1000) 
+      );
+    }
+
+    function moveRange(direction){
+      v = widget_config.tick*direction*8;
+      widget_config.start_timestamp += v;
+      widget_config.end_timestamp += v;
+      redrawWithRange();
+    }
+    
+    function drawLayout(){
+      $(opts.elem).append( $('<div></div>').attr('class', 'headbar').append(
+        $('<div></div>').attr('class', 'button mr').append($('<span></span>').html('refresh')).click(
+          function(){ redrawWithRange(); }
+        )
+      ).append(
+        $('<div></div>').attr('class', 'button mr').append($('<span></span>').html('&rarr;')).click(
+          function(){ moveRange(1); }
+        )
+      ).append(
+        $('<div></div>').attr('class', 'datepicker')
+      ).append(
+        $('<div></div>').attr('class', 'button').append($('<span></span>').html('&larr;')).click(
+          function(){ moveRange(-1); }
+        )
+      ).append(
+        $('<h2></h2>').html(opts.title)
+      ) ).append( $('<div></div>').attr('id', 'container') );
+    }
+
+    function drawChart(){
+      chart = new Highcharts.Chart({     
+        chart: { renderTo: 'container', defaultSeriesType: 'areaspline', height: 270 },
+        series: [],
+        title: { text: '' },
+        xAxis: {       
+          type: 'datetime',
+          tickInterval: opts.tick * 1000, 
+          title: (opts.x_title||''), 
+          labels: { step: 2 } 
+        },
+        yAxis: { 
+          title: (opts.y_title||''), 
+          maxPadding: 0 
+        },
+        legend: {
+          layout: 'horizontal',
+          align: 'top',
+          verticalAlign: 'top',
+          x: -5,
+          y: -3,
+          margin: 25,
+          borderWidth: 0
+        },
+      });
+    }
+
     
     function render(){
 
-      var labels = opts.labels;
+      drawLayout();
+      drawChart();
+
+
+      /*var labels = opts.labels;
       var series = opts.series;
 
       var elem_id = "fm_graph_"+parseInt(Math.random()*99999);
@@ -159,7 +266,7 @@ var FnordMetric = (function(){
 
       });
 
-      canvas.drawGrid(0, 0, width, height, 1, 6, "#ececec");
+      canvas.drawGrid(0, 0, width, height, 1, 6, "#ececec");*/
 
     }
 
@@ -175,13 +282,13 @@ var FnordMetric = (function(){
     var feedInnerElem = $('<ul class="feed_inner"></ul>');
     var typeListElem  = $('<ul class="event_type_list"></ul>');
     var filterElem = $('<div class="events_sidebar"></div>').html(
-      $('<div class="headbar"></div>').html('Event Types')
+      $('<div class="headbar small"></div>').html('Event Types')
     ).append(typeListElem);
     var feedElem = $('<div class="sessions_feed"></div>').html(
-      $('<div class="headbar"></div>').html('Event Feed')
+      $('<div class="headbar small"></div>').html('Event Feed')
     ).append(feedInnerElem);
     var sideElem = $('<div class="sessions_sidebar"></div>').html(
-      $('<div class="headbar"></div>').html('Active Users')
+      $('<div class="headbar small"></div>').html('Active Users')
     ).append(listElem);
 
     var eventsPolledUntil = false;
@@ -453,11 +560,7 @@ var FnordMetric = (function(){
     function renderWidgets(_widgets){
       for(wkey in _widgets){
         var widget = _widgets[wkey];
-        widget["elem"] = $('<div class="widget"></div>').append(
-          $('<div class="headbar"></div>').html(widget.title)
-        ).append(
-          $('<div class="inner"></div>')
-        );
+        widget["elem"] = $('<div class="widget"></div>');
         widgets[wkey] = widget;
         viewport.append(widget.elem);
         resizeWidget(wkey);
