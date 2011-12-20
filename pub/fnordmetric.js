02ee7c6d7ace3f98acde86a6ae5f91472642de32
Paul Asmuth <paul@paulasmuth.com>
2011-12-20 15:05:36 +0100

    bars_widget

--- pub/fnordmetric.js
+++ pub/fnordmetric.js
@@ -450,6 +450,136 @@ var FnordMetric = (function(){
 
   };
 
+
+  var barsWidget = function(){
+    
+    function render(opts){
+      
+      var widget_uid = getNextWidgetUID();
+      var chart=false;
+      var max_y=0;
+
+      function redraw(first_time, silent){
+        if(!silent){ $(opts.elem).css('opacity', 0.5); }
+        max_y=0;
+        $(opts.gauges).each(function(i,gauge){
+          $.ajax({
+            url: '/'+currentNamespace+'/gauge/'+gauge, 
+            success: redrawGauge(first_time, gauge)
+          });         
+        });
+      }
+
+      function redrawGauge(first_time, gauge){
+        return (function(json){                   
+          var raw_data = JSON.parse(json);
+          var series_data = [];
+          var series_type;
+          var label_data = [];
+
+          if(opts.plot_style == 'horizontal'){
+            series_type = 'bar';
+          } else {
+            series_type = 'column';
+          }
+
+          if(opts.order_by == 'field'){
+            raw_data.values.sort(function(a,b){
+              if(a[0] == b[0]){
+                return 0;
+              }else if(a[0] > b[0]){ 
+                return 1;
+              } else {
+                return -1;
+              }
+            });
+          }
+          
+          for(p in raw_data.values){ 
+            label_data.push(raw_data.values[p][0]||'?'); 
+            series_data.push(parseInt(raw_data.values[p][1]||0)); 
+            //max_y = Math.max(max_y, raw_data[p]);
+          }
+  
+          chart = new Highcharts.Chart({     
+            chart: { 
+              renderTo: 'container-'+widget_uid, 
+              defaultSeriesType: series_type, 
+              height: 270 
+            },
+            series: [],
+            title: { text: '' },
+            xAxis: {       
+             categories: label_data
+            },
+            yAxis: {
+              title: { text: '' }
+            },
+            legend: {
+              layout: 'horizontal',
+              align: 'top',
+              verticalAlign: 'top',
+              x: -5,
+              y: -3,
+              margin: 25,
+              borderWidth: 0
+            },
+            plotOptions: {
+              column: {
+                animation: first_time,
+              }
+            },
+            series: [
+              {
+                name: opts.gauge_titles[gauge],
+                data: series_data
+              }
+            ]
+          });
+
+          //chart.redraw();
+          $(opts.elem).css('opacity', 1);
+        });
+      }
+    
+      function drawLayout(){
+        $(opts.elem).append( $('<div></div>').attr('class', 'headbar').append(
+          $('<div></div>').attr('class', 'button mr').append($('<span></span>').html('refresh')).click(
+            function(){ redraw(); }
+          )
+        ).append(
+          $('<h2></h2>').html(opts.title)
+        ) ).append( 
+          $('<div></div>').attr('id', 'container-'+widget_uid).css({
+            height: 256,
+            marginBottom: 20,
+            overflow: 'hidden'
+          })
+        );
+      }
+
+      drawLayout();
+      redraw(true);
+
+      if(opts.autoupdate){  
+        var autoupdate_interval = window.setInterval(function(){
+          redraw(false, true);
+        }, opts.autoupdate*1000);
+
+        $('body').bind('fm_dashboard_close', function(){
+          window.clearInterval(autoupdate_interval);
+        });  
+      }
+
+    }
+
+    return {
+      render: render  
+    };
+
+  };
+
+
   var sessionView = (function(){
     
     var listElem = $('<ul class="session_list"></ul>');
@@ -746,6 +876,7 @@ var FnordMetric = (function(){
       var widget = widgets[wkey];
       /* argh... */
       if(widget.klass=='TimelineWidget'){ timelineWidget().render(widget); }
+      if(widget.klass=='BarsWidget'){ barsWidget().render(widget); }
       if(widget.klass=='NumbersWidget'){ numbersWidget().render(widget); }
       if(widget.klass=='ToplistWidget'){ toplistWidget().render(widget); }
     };
