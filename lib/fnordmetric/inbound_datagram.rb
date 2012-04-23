0b403c5b30d594f3f76edb8781236fb9359b1037
Ross Kaffenberger <rosskaff@gmail.com>
2012-04-23 08:46:23 -0400

    inbound datagram push events to redis api on EM.next_tick

--- lib/fnordmetric/inbound_datagram.rb
+++ lib/fnordmetric/inbound_datagram.rb
@@ -9,12 +9,27 @@ class FnordMetric::InboundDatagram < EventMachine::Connection
     EM.open_datagram_socket(*opts[:inbound_stream], self, opts)
   end
 
-  def receive_data(data)
-    api.event(data)
+  def receive_data(event)
+    events << event
+    push_next_event
+  end
+
+  def push_next_event
+    return true if events.empty?
+    api.event(@events.pop)
+    EM.next_tick(&method(:push_next_event))
+  end
+
+  def unbind
+    api.disconnect
   end
 
   def api
     @api ||= FnordMetric::API.new(self.class.opts)
+  end
+
+  def events
+    @events ||= []
   end
 
 end
