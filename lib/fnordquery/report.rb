4352428907d2a7ee5cac6b2c8b25426a4599ce00
Paul Asmuth <paul@paulasmuth.com>
2012-05-13 07:39:15 +0200

    numtimeseries report wip

--- lib/fnordquery/report.rb
+++ lib/fnordquery/report.rb
@@ -1,4 +1,7 @@
 class FnordQuery::Report
 
+  def initialize(opts)
+  	@opts = opts
+  end
 
 end
--- lib/fnordquery/reports/numeric_timeseries_report.rb
+++ lib/fnordquery/reports/numeric_timeseries_report.rb
@@ -1,7 +1,41 @@
 class FnordQuery::NumericTimeseriesReport < FnordQuery::Report
 
-  def execute(runner, backend)
-  	runner.send(:shutdown, true)
+  def execute(runner, _backend)
+  	backend = _backend[0].new(_backend[1])
+
+  	t_since = @opts["since"].to_i
+  	t_until = @opts["until"].to_i
+
+  	@tick = (t_until - t_since) / 150
+
+  	@timeline = Hash[149.times.inject([t_until]) do |a, n|
+  	  a + [a.last - n*@tick]
+  	end.map do |t|
+  	  [t, 0]
+	end]
+
+  	query = FnordQuery::Query.new("#{@opts["query"]} since(#{t_since}) until(#{t_until})")
+
+  	backend.on_finish do
+  	  render_result
+  	  runner.send(:shutdown, true)
+  	end
+
+  	backend.subscribe(query) do |event|
+
+  	  (event["_time"]-@tick..event["_time"]).to_a.detect do |t|
+  	  	if @timeline.has_key?(t)
+          @timeline[t] += 1
+  	  	end
+  	  end
+
+  	end
+  end
+
+private
+
+  def render_result
+  	puts @timeline.inspect
   end
 
 end
