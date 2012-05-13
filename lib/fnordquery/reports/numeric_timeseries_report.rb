9e9c569b7453776d471666656b5c88bcc390bbb6
Paul Asmuth <paul@paulasmuth.com>
2012-05-13 08:20:40 +0200

    first report generated :)

--- lib/fnordquery/reports/numeric_timeseries_report.rb
+++ lib/fnordquery/reports/numeric_timeseries_report.rb
@@ -4,18 +4,32 @@ class FnordQuery::NumericTimeseriesReport < FnordQuery::Report
   	super
   	backend = _backend[0].new(_backend[1])
 
+    colors = ["#CD645A", "#2F635E", "#FACE4F", "#42436B"]
+
   	t_since = @opts["since"].to_i
   	t_until = @opts["until"].to_i
 
   	@tick = (t_until - t_since) / 150
 
-  	@timeline = Hash[149.times.inject([t_until]) do |a, n|
-  	  a + [a.last - n*@tick]
+    @series_timelines = Hash.new
+    @series_queries = Hash.new
+
+  	timeline = Hash[149.times.inject([t_until]) do |a, n|
+  	  a + [a.first - n*@tick]
   	end.map do |t|
   	  [t, 0]
-	end]
+	  end]
+
+    @opts["series"].each do |skey, series|
+      @opts["series"][skey]["color"] ||= colors.unshift(colors.pop).first
+      @series_timelines[skey] = timeline.dup
+      @series_queries[skey] = FnordQuery::Query.new("#{series["query"]}")
+    end
 
   	query = FnordQuery::Query.new("#{@opts["query"]} since(#{t_since}) until(#{t_until})")
+
+    puts "tick: #{@tick}"
+    puts "time: #{Time.at(t_since).to_s}-#{Time.at(t_until).to_s}"
 
   	backend.on_finish do
   	  render_result
@@ -24,11 +38,16 @@ class FnordQuery::NumericTimeseriesReport < FnordQuery::Report
 
   	backend.subscribe(query) do |event|
 
-  	  (event["_time"]-@tick..event["_time"]).to_a.detect do |t|
-  	  	if @timeline.has_key?(t)
-          @timeline[t] += 1
-  	  	end
-  	  end
+      @series_queries.each do |skey, squery|
+        if squery.matches?(event)
+          #puts event.inspect
+  	      (event["_time"]-@tick..event["_time"]).to_a.reverse.detect do |t|
+  	  	    if @series_timelines[skey].has_key?(t)
+              @series_timelines[skey][t] += 1
+  	  	    end
+  	      end
+        end
+      end
 
   	end
   end
@@ -36,6 +55,13 @@ class FnordQuery::NumericTimeseriesReport < FnordQuery::Report
 private
 
   def render_result
+    @series_render = @opts["series"].map do |skey, sopts|
+      {
+        :data  => @series_timelines[skey].map { |t,v| { :x => t, :y => v } },
+        :name  => sopts["name"],
+        :color => sopts["color"]
+      }
+    end
   	render_haml(:numeric_timeseries_report, 'report.html')
   end
 
