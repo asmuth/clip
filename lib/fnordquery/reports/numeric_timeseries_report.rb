fb78dc1d405d2bc7799b13278d37b4b07c00c045
Paul Asmuth <paul@paulasmuth.com>
2012-05-14 02:18:06 +0200

    categorical topk wip

--- lib/fnordquery/reports/numeric_timeseries_report.rb
+++ lib/fnordquery/reports/numeric_timeseries_report.rb
@@ -1,24 +1,24 @@
 class FnordQuery::NumericTimeseriesReport < FnordQuery::Report
 
   def execute(runner, _backend)
-  	super
-  	backend = _backend[0].new(_backend[1])
+    super
+    backend = _backend[0].new(_backend[1])
 
     colors = ["#CD645A", "#2F635E", "#FACE4F", "#42436B"]
 
-  	t_since = @opts["since"].to_i
-  	t_until = @opts["until"].to_i
+    t_since = @opts["since"].to_i
+    t_until = @opts["until"].to_i
 
-  	@tick = (t_until - t_since) / 150
+    @tick = (t_until - t_since) / 150
 
     @series_timelines = Hash.new
     @series_queries = Hash.new
 
-  	timeline = Hash[149.times.inject([t_until]) do |a, n|
-  	  a + [a.first - n*@tick]
-  	end.map do |t|
-  	  [t, 0]
-	  end]
+    timeline = Hash[149.times.inject([t_until]) do |a, n|
+      a + [a.first - n*@tick]
+    end.map do |t|
+      [t, 0]
+    end]
 
     @opts["series"].each do |skey, series|
       @opts["series"][skey]["color"] ||= colors.unshift(colors.pop).first
@@ -26,29 +26,29 @@ class FnordQuery::NumericTimeseriesReport < FnordQuery::Report
       @series_queries[skey] = FnordQuery::Query.new("#{series["query"]}")
     end
 
-  	query = FnordQuery::Query.new("#{@opts["query"]} since(#{t_since}) until(#{t_until})")
+    query = FnordQuery::Query.new("#{@opts["query"]} since(#{t_since}) until(#{t_until})")
 
     puts "tick: #{@tick}"
     puts "time: #{Time.at(t_since).to_s}-#{Time.at(t_until).to_s}"
 
-  	backend.on_finish do
-  	  render_result
-  	  runner.send(:shutdown, true)
-  	end
+    backend.on_finish do
+      render_result
+      runner.send(:shutdown, true)
+    end
 
-  	backend.subscribe(query) do |event|
+    backend.subscribe(query) do |event|
 
       @series_queries.each do |skey, squery|
         if squery.matches?(event)
-  	      (event["_time"]-@tick..event["_time"]).to_a.reverse.detect do |t|
-  	  	    if @series_timelines[skey].has_key?(t)
+          (event["_time"]-@tick..event["_time"]).to_a.reverse.detect do |t|
+            if @series_timelines[skey].has_key?(t)
               @series_timelines[skey][t] += 1
-  	  	    end
-  	      end
+            end
+          end
         end
       end
 
-  	end
+    end
   end
 
 private
@@ -63,7 +63,7 @@ private
         :color => sopts["color"]
       }
     end
-  	render_haml(:numeric_timeseries_report, 'report.html')
+    render_haml(:numeric_timeseries_report, 'report.html')
   end
 
 end
