3fc14e2577d0fefe19fffaef2afc007d919df988
Paul Asmuth <paul@paulasmuth.com>
2012-05-19 01:28:18 +0200

    bars widget stub

--- lib/fnordmetric/gauges/toplist_gauge.rb
+++ lib/fnordmetric/gauges/toplist_gauge.rb
@@ -4,16 +4,30 @@ class FnordMetric::ToplistGauge < FnordMetric::Gauge
     interval = parse_interval(event["interval"])
     colors = ["#2F635E", "#606B36", "#727070", "#936953", "#CD645A", "#FACE4F", "#42436B"]
 
+    #res = @opts[:redis].zrevrangebyscore(rkey, "+inf", 0, :limit => [0, @opts[:show_top_n]]) 
 
     render_page(:toplist_gauge)
   end
 
   def execute(cmd, context, *args)
-    #incr_series(context, *args) if cmd == :incr
+    return observe(context, args.first) if cmd == :observe
+    FnordMetric.error("gauge '#{name}': unknown command: #{cmd}")
   end
 
   def renderable?
     true
+  end
+
+private
+
+  def observe(ctx, item)
+    at = ctx.send(:time)
+    ctx.redis_exec :zincrby, tick_key(at, :toplist), 1, item
+    ctx.redis_exec :zremrangebyrank, tick_key(at, :toplist), 0, -top_k
+  end
+
+  def top_k
+    (@opts[:top_k] || 1000).to_i
   end
 
 end
--- web/fnordmetric.bars_widget.js
+++ web/fnordmetric.bars_widget.js
@@ -1,5 +1,223 @@
 FnordMetric.widgets.barsWidget = function(){
 
+    var widget_uid = "fnord-" + parseInt(Math.random()*99990000);
+    var width, height, opts, graph, gconfig, legend, hoverDetail, shelving, highlighter, values;
+
+    var cardinal = true;
+
+    function render(_opts){
+      opts = _opts;
+
+      if(!opts.default_style){ opts.default_style = 'line'; }
+      if(!opts.default_cardinal){ opts.default_cardinal = true; }
+
+      draw_layout();
+
+      width = opts.elem.width() - 50;
+      height = opts.height || 240;
+
+      gconfig = {
+        element: $('.container', opts.elem)[0],
+        renderer: "line",
+        width: width,
+        height: height,
+        padding: { top: 0.1, bottom: 0 }
+      }
+
+      if(opts.async_chart){
+        updateChart();
+      } else {
+        values = opts.values;
+        renderChart();
+      }
+    }
+
+
+    function draw_layout(){
+      $(opts.elem)
+        .append(
+          $('<div></div>')
+            .addClass('headbar')
+            .append($('<h2></h2>').html(opts.title))
+        )
+        .append(
+          $('<div></div>')
+            .addClass('legend')
+            .css({
+              margin: '10px 30px 0 30px',
+            })
+        )
+        .append(
+          $('<div></div>')
+            .addClass('container')
+            .css({
+              height: opts.height,
+              margin: '0 23px 25px 23px',
+            })
+        );
+      
+      if(opts.async_chart){
+        $('.headbar', opts.elem).prepend(
+          $('<div></div>')
+            .addClass('button ml')
+            .append($('<span></span>').html('&larr;'))
+            .click(function(){ moveRange(-1); })
+        ).prepend(
+          $('<div></div>')
+            .addClass('datepicker')
+        ).prepend(
+          $('<div></div>')
+            .addClass('button')
+            .append($('<span></span>').html('&rarr;'))
+            .click(function(){ moveRange(1); })
+        );
+      }
+    }
+
+    function renderChart(){
+      $(gconfig.element).html("");
+      $(".rickshaw_legend", opts.elem).html("");
+
+      gconfig.series = [{
+        name: "Fnord",
+        color: "#0066CC",
+        data: []
+      }];
+
+      for(var n=0; n < values.length; n++){
+        gconfig.series[0].data.push({
+          x: values[n][0],
+          y: values[n][1]
+        });
+      }
+
+      graph = new Rickshaw.Graph(gconfig);
+
+      legend = new Rickshaw.Graph.Legend({
+        graph: graph,
+        element: $('.legend', opts.elem)[0]
+      });
+
+      hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        graph: graph
+      });
+
+      shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+        graph: graph,
+        legend: legend
+      });
+
+      highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
+        graph: graph,
+        legend: legend
+      });
+
+      new Rickshaw.Graph.Axis.Time({
+        graph: graph,
+      }).render();
+
+      new Rickshaw.Graph.Axis.Y({
+        graph: graph,
+      }).render();
+
+      graph.configure(gconfig);
+      graph.render();
+    }
+
+
+    function announce(evt){
+      if(evt.widget_key == opts.widget_key){
+        if((evt.class == "widget_response") && (evt.cmd == "values_at")){
+          running_request = false;
+          $(opts.elem).css('opacity', 1); 
+          updateSeriesData(evt.gauges);
+        }
+      }
+    }
+
+    function requestValuesAsync(){
+      FnordMetric.publish({
+        "type": "widget_request",
+        "klass": "TimeseriesWidget",
+        "gauges": opts.gauges,
+        "cmd": "values_at",
+        "tick": opts.tick, 
+        "since": opts.start_timestamp,
+        "until": opts.end_timestamp,
+        "widget_key": opts.widget_key
+      })
+    }
+
+    function updateSeriesData(dgauges){
+      // gconfig.series = [];
+
+      // for(var ind = 0; ind < dgauges.length; ind++){
+        
+      //   gconfig.series.push({
+      //     name: dgauges[ind].key,
+      //     color: opts.series[ind].color,
+      //     data: []
+      //   });
+        
+      //   for(_time in dgauges[ind].vals){
+      //     gconfig.series[ind].data.push(
+      //       { x: parseInt(_time), y: parseInt(dgauges[ind].vals[_time] || 0) }
+      //     );
+      //   }
+
+      // }
+
+      // renderChart();
+    }
+
+    function updateChart(first_time, silent){
+      if(!silent){ $(opts.elem).css('opacity', 0.5); }    
+      updateRange();
+      redrawDatepicker();
+      requestValuesAsync();
+    }
+
+    function redrawDatepicker(){
+      // dateFormat('%d.%m.%y %H:%M', parseInt(opts.start_timestamp)*1000) +
+      $('.datepicker', opts.elem).html(
+        opts.start_timestamp +
+        '&nbsp;&dash;&nbsp;' +
+        opts.end_timestamp
+      );
+    }
+
+    function updateRange(force){
+      if(!opts.tick){
+        opts.tick = opts.ticks[0];
+      }
+
+      if(!opts.start_timestamp || !opts.end_timestamp || !!force){
+        opts.end_timestamp = parseInt(new Date().getTime() / 1000);
+        opts.start_timestamp = opts.end_timestamp - (opts.tick * xticks);
+      }
+    }
+
+    function moveRange(direction){
+      v = opts.tick*direction*8;
+
+      if(((opts.end_timestamp + v)*1000) < new Date().getTime()){
+        opts.start_timestamp += v;
+        opts.end_timestamp += v;  
+      }
+
+      updateChart();
+    }
+
+    return {
+      render: render,
+      announce: announce
+    }
+
+};
+
+/*
+FnordMetric.widgets.barsWidget = function(){
+
   function render(opts){
 
     var widget_uid = FnordMetric.util.getNextWidgetUID();
@@ -127,3 +345,4 @@ FnordMetric.widgets.barsWidget = function(){
   };
 
 };
+*/
