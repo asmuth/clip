efe6e6ba5a43dfd1aad6b63d9606d326610b6275
Paul Asmuth <paul@paulasmuth.com>
2012-05-23 21:47:36 +0200

    full_example

--- doc/full_example.rb
+++ doc/full_example.rb
@@ -5,75 +5,100 @@ FnordMetric.options = {
   :event_queue_ttl  => 10, # all data that isn't processed within 10s is discarded to prevent memory overruns
   :event_data_ttl   => 10,
   :session_data_ttl => 1,  # we don't care about session data for now
-  :redis_prefix => "fnordmetric-dawanda" 
+  :redis_prefix => "fnordmetric" 
 }
 
-FnordMetric.namespace :ulm do
-
-  gauge :skip_votes, :tick => 1.day.to_i, :title => "Skip-Votes"
-  gauge :yes_votes, :tick => 1.day.to_i, :title => "Yes-Votes"
-  gauge :maybe_votes, :tick => 1.day.to_i, :title => "Maybe-Votes"
-
-  gauge :messages_sent, :tick => 1.day.to_i, :title => "Messages (sent)"
-  gauge :messages_read, :tick => 1.day.to_i, :title => "Messages (read)"
-  gauge :winks_sent, :tick => 1.day.to_i, :title => "Winks sent"
 
 
-  timeseries_gauge :gross_search_volume,
-    :tick => 30.seconds,
-    :group => "Search",
-    :title => "Gross Search Volume",
-    :key_nouns => ["Search", "Searches"],
-    :series => [:without_filter, :with_filter]
-    #:enable_punchcard => true,
-    #:enable_realtime => true
+
+FnordMetric.namespace :myapp do
 
 
-  widget 'Overview', {
-    :title => "Yes/No/Skip-Votes",
-    :type => :timeline,
-    :gauges => [:skip_votes, :yes_votes, :maybe_votes]
-  }
+  timeseries_gauge :number_of_signups,
+    :title => "Number of Signups",
+    :series => [:signup_via_twitter, :signup_via_openid],
+    :resolution => 30.seconds
 
-  widget 'Overview', {
-    :title => "Yes/No/Skip Numbers",
-    :type => :numbers,
-    :gauges => [:skip_votes, :yes_votes, :maybe_votes]
-  }
 
-  widget 'Overview', {
-    :title => "Messages sent/read",
-    :type => :timeline,
-    :gauges => [:messages_sent, :messages_read, :winks_sent]
-  }
+  distribution_gauge :user_age_distribution,
+    :title => "User Age Distribution",
+    :resolution => 30.seconds
 
-  widget 'Overview', {
-    :title => "Messages sent+read/Winks Numbers",
-    :type => :numbers,
-    :gauges => [:messages_sent, :messages_read, :winks_sent]
-  }
+
+  toplist_gauge :popular_keywords,
+    :title => "Popular Keywords",
+    :resolution => 30.seconds
   
-  widget 'Overview', {
-    :title => "User-Activity",
+
+  gauge :events_per_hour, :tick => 1.hour
+  gauge :events_per_second, :tick => 1.second
+  gauge :events_per_minute, :tick => 1.minute
+
+  event :"*" do
+    incr :events_per_hour
+    incr :events_per_minute
+    incr :events_per_second
+  end
+
+  widget 'TechStats', {
+    :title => "Events per Minute",
     :type => :timeline,
-    :width => 67,
-    :autoupdate => 30,
-    :gauges => [:skip_votes, :yes_votes, :maybe_votes]
+    :width => 100,
+    :gauges => :events_per_minute,
+    :include_current => true,
+    :autoupdate => 30
+  }
+
+  widget 'TechStats', {
+    :title => "Events per Hour",
+    :type => :timeline,
+    :width => 50,
+    :gauges => :events_per_hour,
+    :include_current => true,
+    :autoupdate => 30
+  }
+
+
+  widget 'TechStats', {
+    :title => "Events/Second",
+    :type => :timeline,
+    :width => 50,
+    :gauges => :events_per_second,
+    :include_current => true,
+    :plot_style => :areaspline,
+    :autoupdate => 1
+  }
+
+  widget 'TechStats', {
+    :title => "Events Numbers",
+    :type => :numbers,
+    :width => 100,
+    :gauges => [:events_per_second, :events_per_minute, :events_per_hour],
+    :offsets => [1,3,5,10],
+    :autoupdate => 1
   }
 
 end
 
-FnordMetric.options = {
-  :print_stats => 3
-}
 
-FnordMetric::Logger.new(
-  :file => '/home/paul/fnordmetric.log',
-  :channels => ["dawanda-firehose"]
-)
+def start_example_data_generator
+
+  api = FnordMetric::API.new
+  Thread.new do
+    loop do      
+      api.event(:_type => :unicorn_seen)
+      sleep rand(10)/10.to_f
+    end
+  end
+
+end
+
+start_example_data_generator
 
 FnordMetric::Web.new(:port => 4242)
-
 FnordMetric::Acceptor.new(:protocol => :tcp, :port => 2323)
-
+FnordMetric::Worker.new
 FnordMetric.run
+
+
+
