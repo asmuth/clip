fe4bb8be0b221098d9a6a980434642e61166ac56
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2013-02-16 15:51:01 +0700

    Use let block instead of setup to be able to overwrite it in nested contexts.

--- fnordmetric-core/spec/worker_spec.rb
+++ fnordmetric-core/spec/worker_spec.rb
@@ -2,24 +2,22 @@ require ::File.expand_path('../spec_helper.rb', __FILE__)
 
 describe FnordMetric::Worker do
 
-  before(:each) do
-    @worker = FnordMetric::Worker.new()
-  end
- 
+  let(:worker) { FnordMetric::Worker.new() }
+
   it "should generate the correct pubsub-key" do
-    @worker.pubsub_key.should == "fnordmetric-announce"
+    worker.pubsub_key.should == "fnordmetric-announce"
   end
 
   it "should generate the correct queue-key" do
-    @worker.queue_key.should == "fnordmetric-queue"
+    worker.queue_key.should == "fnordmetric-queue"
   end
 
   it "should generate the correct event-key" do
-    @worker.event_key("myevent").should == "fnordmetric-event-myevent"
+    worker.event_key("myevent").should == "fnordmetric-event-myevent"
   end
 
   it "should generate the correct stats-key" do
-    @worker.stats_key.should == "fnordmetric-stats"
+    worker.stats_key.should == "fnordmetric-stats"
   end
 
   it "should add a namespace"  
