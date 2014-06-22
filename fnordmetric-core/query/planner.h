187f34040ef5ac359d067a1d4f0439edf379dc19
Paul Asmuth <paul@paulasmuth.com>
2014-06-22 17:16:43 +0100

    stub expr execution

--- fnordmetric-core/query/planner.h
+++ fnordmetric-core/query/planner.h
@@ -16,29 +16,149 @@
 namespace fnordmetric {
 namespace query {
 
+class SValue {
+public:
+  enum kSValueType {
+    T_STRING,
+    T_FLOAT,
+    T_INTEGER
+  };
+
+  explicit SValue(int64_t integer_value) : type_(T_INTEGER) {
+    data_.t_integer = integer_value;
+  }
+
+  explicit SValue(double float_value);
+  explicit SValue(const char* str_value, size_t len, bool copy);
+  SValue(const SValue& copy);
+  SValue& operator=(const SValue& copy) = delete;
+
+  ~SValue() {
+  }
+
+  static SValue* fromToken(const Token* token) {
+    switch (token->getType()) {
+
+      case Token::T_NUMERIC: {
+        return new SValue((int64_t) 123);
+      }
+
+      default:
+        return nullptr;
+
+    }
+  }
+
+  kSValueType getType() const {
+    return type_;
+  }
+
+  int64_t getInteger() const {
+    assert(type_ == T_INTEGER);
+    return data_.t_integer;
+  }
+
+protected:
+  kSValueType type_;
+  union {
+    int64_t t_integer;
+    double t_float;
+    struct {
+      const char* begin;
+      const char* end;
+      bool owned;
+    } t_string;
+  } data_;
+};
+
 class Executable {
 public:
   virtual ~Executable() {}
   virtual void execute() = 0;
 protected:
-  void expr(ASTNode* expr) {
-    printf("execute expression %p\n", expr);
 
-    expr->debugPrint(2);
-    switch (expr->getType()) {
+  SValue* expr(ASTNode* e) {
+    printf("execute expression %p\n", e);
+
+    e->debugPrint(2);
+    switch (e->getType()) {
       case ASTNode::T_ADD_EXPR:
-        return addExpr(expr);
+        return addExpr(e);
+
+      case ASTNode::T_MUL_EXPR:
+        return mulExpr(e);
+
+      case ASTNode::T_LITERAL:
+        assert(e->getToken() != nullptr);
+        return SValue::fromToken(e->getToken());
 
       default:
         assert(0); // FIXPAUL
     };
   }
 
-  void addExpr(ASTNode* expr) {
-    auto args = expr->getChildren();
+  SValue* addExpr(ASTNode* e) {
+    auto args = e->getChildren();
     assert(args.size() == 2);
-    printf("addexpr\n");
+
+    std::unique_ptr<SValue> lhs(expr(args[0]));
+    std::unique_ptr<SValue> rhs(expr(args[1]));
+
+    if (lhs.get() == nullptr || rhs.get() == nullptr) {
+      return nullptr;
+    }
+
+    SValue* result = nullptr;
+    switch(lhs->getType()) {
+
+      case SValue::T_INTEGER: {
+        switch(rhs->getType()) {
+          case SValue::T_INTEGER: {
+            result = new SValue(lhs->getInteger() + rhs->getInteger());
+          }
+        }
+      }
+    }
+
+    if (result == nullptr) {
+      assert(0); // FIXPAUL
+    } else {
+      return result;
+    }
   }
+
+  SValue* mulExpr(ASTNode* e) {
+    auto args = e->getChildren();
+    assert(args.size() == 2);
+
+    std::unique_ptr<SValue> lhs(expr(args[0]));
+    std::unique_ptr<SValue> rhs(expr(args[1]));
+
+    if (lhs.get() == nullptr || rhs.get() == nullptr) {
+      return nullptr;
+    }
+
+    SValue* result = nullptr;
+    switch(lhs->getType()) {
+
+      case SValue::T_INTEGER: {
+        switch(rhs->getType()) {
+          case SValue::T_INTEGER: {
+            result = new SValue(lhs->getInteger() * rhs->getInteger());
+          }
+        }
+      }
+    }
+
+    if (result == nullptr) {
+      assert(0); // FIXPAUL
+    } else {
+      return result;
+    }
+  }
+
+
+
 };
 
 class TablelessSelect : public Executable {
@@ -64,9 +184,9 @@ public:
 
   void execute() override {
     for (const auto& col : columns_) {
-      printf("execute col=%s\n",
-          col.first.c_str(),
-          expr(col.second));
+      std::unique_ptr<SValue> value(expr(col.second));
+
+      printf("execute col=%s=%i\n",col.first.c_str(), value->getInteger());
     }
   }
 
