c94769bc8338ae2f08ce93b7caeb843eae3339a6
Paul Asmuth <paul@paulasmuth.com>
2014-07-19 23:34:23 +0100

    simple area chart

---
+++ areachart.cc
@@ -0,0 +1,207 @@
+/**
+ * This file is part of the "FnordMetric" project
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * Licensed under the MIT license (see LICENSE).
+ */
+#include <stdlib.h>
+#include "canvas.h"
+#include "areachart.h"
+#include "rendertarget.h"
+
+namespace fnordmetric {
+namespace ui {
+
+char AreaChart::kDefaultBorderStyle[] = "none";
+double AreaChart::kDefaultBorderWidth = 2.0f;
+char AreaChart::kDefaultPointStyle[] = "none";
+double AreaChart::kDefaultPointSize = 3.0f;
+
+AreaChart::AreaChart(
+    Canvas* canvas,
+    NumericalDomain* x_domain /* = nullptr */,
+    NumericalDomain* y_domain /* = nullptr */,
+    bool stacked /* = false */) :
+    canvas_(canvas),
+    x_domain_(x_domain),
+    y_domain_(x_domain),
+    stacked_(stacked),
+    num_series_(0) {}
+
+void AreaChart::addSeries(
+      Series2D<double, double>* series,
+      const std::string& border_style /* = kDefaultBorderStyle */,
+      double border_width /* = kDefaultBorderWidth */,
+      const std::string& point_style /* = kDefaultPointStyle */,
+      double point_size /* = kDefaultPointSize */,
+      bool smooth /* = false */) {
+  Area area;
+  area.color = seriesColor(series);
+  area.border_style = border_style;
+  area.border_width = border_width;
+  area.point_style = point_style;
+  area.point_size = point_size;
+  area.smooth = smooth;
+
+  // FIXPAUL: stacked
+  for (const auto& spoint : series->getData()) {
+    area.points.emplace_back(std::get<0>(spoint), 0, std::get<1>(spoint));
+  }
+
+  areas_.emplace_back(area);
+}
+
+AxisDefinition* AreaChart::addAxis(AxisDefinition::kPosition position) {
+  switch (position) {
+    case AxisDefinition::TOP:
+      return canvas_->addAxis(position, getXDomain());
+
+    case AxisDefinition::RIGHT:
+      return canvas_->addAxis(position, getYDomain());
+
+    case AxisDefinition::BOTTOM:
+      return canvas_->addAxis(position, getXDomain());
+
+    case AxisDefinition::LEFT:
+      return canvas_->addAxis(position, getYDomain());
+  }
+}
+
+NumericalDomain* AreaChart::getXDomain() const {
+  if (x_domain_ != nullptr) {
+    return x_domain_;
+  }
+
+  if (x_domain_auto_.get() == nullptr) {
+    double x_min = 0.0f;
+    double x_max = 0.0f;
+
+    for (const auto& area : areas_) {
+      for (const auto& point : area.points) {
+        if (std::get<0>(point) > x_max) {
+          x_max = std::get<0>(point);
+        }
+        if (std::get<0>(point) < x_min) {
+          x_min = std::get<0>(point) ;
+        }
+      }
+    }
+
+    if (x_max > 0) {
+      x_max *= 1.1;
+    }
+
+    if (x_max < 0) {
+      x_max *= 0.9;
+    }
+
+    if (x_min > 0) {
+      x_min *= 0.9;
+    }
+
+    if (x_min < 0) {
+      x_min *= 1.1;
+    }
+
+    x_domain_auto_.reset(new NumericalDomain(x_min, x_max, false));
+  }
+
+  return x_domain_auto_.get();
+}
+
+NumericalDomain* AreaChart::getYDomain() const {
+  if (y_domain_ != nullptr) {
+    return y_domain_;
+  }
+
+  if (y_domain_auto_.get() == nullptr) {
+    double y_min = 0.0f;
+    double y_max = 0.0f;
+
+    for (const auto& area : areas_) {
+      for (const auto& point : area.points) {
+        if (std::get<2>(point) > y_max) {
+          y_max = std::get<2>(point);
+        }
+        if (std::get<2>(point) < y_min) {
+          y_min = std::get<2>(point) ;
+        }
+      }
+    }
+
+    if (y_max > 0) {
+      y_max *= 1.1;
+    }
+
+    if (y_max < 0) {
+      y_max *= 0.9;
+    }
+
+    if (y_min > 0) {
+      y_min *= 0.9;
+    }
+
+    if (y_min < 0) {
+      y_min *= 1.1;
+    }
+
+    y_domain_auto_.reset(new NumericalDomain(y_min, y_max, false));
+  }
+
+
+  return y_domain_auto_.get();
+}
+
+void AreaChart::render(
+    RenderTarget* target,
+    int width,
+    int height,
+    std::tuple<int, int, int, int>* padding) const {
+  auto padding_top = std::get<0>(*padding);
+  auto padding_right = std::get<1>(*padding);
+  auto padding_bottom = std::get<2>(*padding);
+  auto padding_left = std::get<3>(*padding);
+  auto inner_width = width - padding_right - padding_left;
+  auto inner_height = height - padding_top - padding_bottom;
+  auto x_domain = getXDomain();
+  auto y_domain = getYDomain();
+
+  target->beginGroup("areas");
+
+  for (const auto& area : areas_) {
+    std::vector<std::pair<double, double>> area_coords;
+
+    for (int i = 0; i < area.points.size(); ++i) {
+      const auto& point = area.points[i];
+      auto scaled_x = x_domain->scale(std::get<0>(point));
+      auto scaled_y2 = 1.0 - y_domain->scale(std::get<2>(point));
+
+      area_coords.emplace_back(
+          padding_left + scaled_x * inner_width,
+          padding_top + scaled_y2 * inner_height);
+    }
+
+    for (int i = area.points.size() - 1; i >= 0; --i) {
+      const auto& point = area.points[i];
+      auto scaled_x = x_domain->scale(std::get<0>(point));
+      auto scaled_y1 = 1.0 - y_domain->scale(std::get<1>(point));
+
+      area_coords.emplace_back(
+          padding_left + scaled_x * inner_width,
+          padding_top + scaled_y1 * inner_height);
+    }
+
+    target->drawPath(
+      area_coords,
+      "fill",
+      area.border_style == "none" ? 0 : area.border_width,
+      area.smooth,
+      area.color,
+      "area");
+  }
+
+  target->finishGroup();
+}
+
+}
+}
---
+++ areachart.h
@@ -0,0 +1,117 @@
+/**
+ * This file is part of the "FnordMetric" project
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * Licensed under the MIT license (see LICENSE).
+ */
+#ifndef _FNORDMETRIC_AREACHART_H
+#define _FNORDMETRIC_AREACHART_H
+#include <stdlib.h>
+#include <assert.h>
+#include "../base/series.h"
+#include "axisdefinition.h"
+#include "domain.h"
+#include "drawable.h"
+
+namespace fnordmetric {
+namespace ui {
+class Canvas;
+class Domain;
+
+/**
+ * This draws the series data as points.
+ *
+ * OPTIONS
+ *
+ *   point_style = {none,circle}; default: none
+ *   point_size = default: 2
+ *   border_style = {none,solid,dashed}, default: none
+ *   border_width = default: 2
+ *
+ */
+class AreaChart : public Drawable {
+public:
+  static char kDefaultPointStyle[];
+  static double kDefaultPointSize;
+  static char kDefaultBorderStyle[];
+  static double kDefaultBorderWidth;
+
+  /**
+   * Create a new area chart
+   *
+   * @param canvas the canvas to draw this chart on. does not transfer ownership
+   * @param stacked stack areas?
+   * @param x_domain the x domain. does not transfer ownership
+   * @param y_domain the y domain. does not transfer ownership
+   */
+  AreaChart(
+      Canvas* canvas,
+      NumericalDomain* x_domain = nullptr,
+      NumericalDomain* y_domain = nullptr,
+      bool stacked = false);
+
+  /**
+   * Add a (x: string, y: double) series. This will draw with height 0 to y
+   * for each x
+   *
+   * @param series the series to add. does not transfer ownership
+   * @param border_style the border style
+   * @param border_width the line width
+   * @param point_style the point style
+   * @param point_width the point size
+   * @param smooth smooth this area?
+   * @param line_style the line style ({solid,dashed})
+   */
+  void addSeries(
+      Series2D<double, double>* series,
+      const std::string& border_style = kDefaultBorderStyle,
+      double border_width = kDefaultBorderWidth,
+      const std::string& point_style = kDefaultPointStyle,
+      double point_size = kDefaultPointSize,
+      bool smooth = false);
+
+  /**
+   * Add an axis to the chart. This method should only be called after all
+   * series have been added to the chart.
+   *
+   * The returned pointer is owned by the canvas object and must not be freed
+   * by the caller!
+   *
+   * @param position the position/placement of the axis
+   */
+   AxisDefinition* addAxis(AxisDefinition::kPosition position);
+
+protected:
+
+  NumericalDomain* getXDomain() const;
+  NumericalDomain* getYDomain() const;
+
+  void render(
+      RenderTarget* target,
+      int width,
+      int height,
+      std::tuple<int, int, int, int>* padding) const override;
+
+  struct Area {
+    std::vector<std::tuple<double, double, double>> points;
+    std::string border_style;
+    double border_width;
+    std::string point_style;
+    double point_size;
+    std::string color;
+    bool smooth;
+  };
+
+  Canvas* canvas_;
+  NumericalDomain* x_domain_;
+  NumericalDomain* y_domain_;
+  bool stacked_;
+  int num_series_;
+  std::vector<Area> areas_;
+  mutable std::unique_ptr<NumericalDomain> x_domain_auto_;
+  mutable std::unique_ptr<NumericalDomain> y_domain_auto_;
+};
+
+}
+}
+#endif
