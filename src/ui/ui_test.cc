647e5aa277430a9d3f43988f078415625d977956
Paul Asmuth <paul@paulasmuth.com>
2014-07-19 15:45:39 +0100

    render right axis

--- src/ui/ui_test.cc
+++ src/ui/ui_test.cc
@@ -23,7 +23,8 @@ public:
   void run() {
     //testCanvasWithLeftAxis();
     //testCanvasWithLeftAxisAndTitle();
-    testCanvasWithLeftAndBottomAxis();
+    //testCanvasWithLeftAndBottomAxis();
+    testCanvasWithAllAxis();
     //testCanvasWithMultiLeftAxis();
   }
 
@@ -100,6 +101,69 @@ public:
     axis_bottom->addLabel(0.4, "B");
     axis_bottom->addLabel(0.6, "C");
     axis_bottom->addLabel(0.8, "D");
+
+    auto svg = canvas.renderSVG();
+    //printf("svg data: %s", svg.c_str());
+  }
+
+  void testCanvasWithAllAxis() {
+    ui::Canvas canvas;
+
+    auto axis_left = canvas.addAxis(ui::AxisDefinition::LEFT);
+    axis_left->setTitle("my axis");
+    axis_left->addTick(0.0);
+    axis_left->addTick(0.2);
+    axis_left->addTick(0.4);
+    axis_left->addTick(0.6);
+    axis_left->addTick(0.8);
+    axis_left->addTick(1.0);
+    axis_left->addLabel(0.0, "0");
+    axis_left->addLabel(0.2, "1");
+    axis_left->addLabel(0.4, "2");
+    axis_left->addLabel(0.6, "3");
+    axis_left->addLabel(0.8, "4");
+    axis_left->addLabel(1.0, "5");
+
+    auto axis_right = canvas.addAxis(ui::AxisDefinition::RIGHT);
+    axis_right->setTitle("my axis");
+    axis_right->addTick(0.0);
+    axis_right->addTick(0.2);
+    axis_right->addTick(0.4);
+    axis_right->addTick(0.6);
+    axis_right->addTick(0.8);
+    axis_right->addTick(1.0);
+    axis_right->addLabel(0.0, "0");
+    axis_right->addLabel(0.2, "1");
+    axis_right->addLabel(0.4, "2");
+    axis_right->addLabel(0.6, "3");
+    axis_right->addLabel(0.8, "4");
+    axis_right->addLabel(1.0, "5");
+
+    auto axis_bottom = canvas.addAxis(ui::AxisDefinition::BOTTOM);
+    axis_bottom->setTitle("my axis");
+    axis_bottom->addTick(0.0);
+    axis_bottom->addTick(0.2);
+    axis_bottom->addTick(0.4);
+    axis_bottom->addTick(0.6);
+    axis_bottom->addTick(0.8);
+    axis_bottom->addTick(1.0);
+    axis_bottom->addLabel(0.2, "A");
+    axis_bottom->addLabel(0.4, "B");
+    axis_bottom->addLabel(0.6, "C");
+    axis_bottom->addLabel(0.8, "D");
+
+    auto axis_top = canvas.addAxis(ui::AxisDefinition::TOP);
+    axis_top->setTitle("my axis");
+    axis_top->addTick(0.0);
+    axis_top->addTick(0.2);
+    axis_top->addTick(0.4);
+    axis_top->addTick(0.6);
+    axis_top->addTick(0.8);
+    axis_top->addTick(1.0);
+    axis_top->addLabel(0.2, "A");
+    axis_top->addLabel(0.4, "B");
+    axis_top->addLabel(0.6, "C");
+    axis_top->addLabel(0.8, "D");
 
     auto svg = canvas.renderSVG();
     //printf("svg data: %s", svg.c_str());
