eaa874da8ee450c6a36adfc7a76623105301e51b
Paul Asmuth <paul@paulasmuth.com>
2014-09-01 00:38:25 +0100

    cleaning up...

--- doc/examples/linecharts/multiple_y_axes.sql
+++ doc/examples/linecharts/multiple_y_axes.sql
@@ -1,28 +1,17 @@
 IMPORT TABLE city_temperatures
-   FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
+    FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
 
 DRAW LINECHART WITH
-  AXIS BOTTOM
-  AXIS LEFT;
+    AXIS BOTTOM
+    AXIS LEFT;
 
-SELECT
-  city AS series,
-  month AS x,
-  temperature AS y
-FROM
-  city_temperatures
-WHERE
-  city = "New York";
+SELECT city AS series, month AS x, temperature AS y, "circle" AS pointstyle
+    FROM city_temperatures
+    WHERE city = "New York";
 
 DRAW LINECHART WITH
-  AXIS RIGHT;
+    AXIS RIGHT;
 
-SELECT
-  "fnord" AS series,
-  month AS x,
-  temperature AS y,
-  "color2" as color
-FROM
-  city_temperatures
-WHERE
-  city = "Berlin";
+SELECT city AS series, month AS x, temperature AS y, "circle" as pointstyle, "color2" as color
+    FROM city_temperatures
+    WHERE city = "Berlin";
