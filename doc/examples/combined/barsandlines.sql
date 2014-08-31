f76507204254260f27cb8489385fe2510fb65962
Paul Asmuth <paul@paulasmuth.com>
2014-08-31 22:54:19 +0100

    combined charts :)

---
+++ doc/examples/combined/barsandlines.sql
@@ -0,0 +1,29 @@
+IMPORT TABLE city_temperatures
+   FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
+
+DRAW LINECHART WITH
+  AXIS BOTTOM
+  AXIS LEFT;
+
+SELECT
+  city AS series,
+  month AS x,
+  temperature AS y
+FROM
+  city_temperatures
+WHERE
+  city = "Tokyo";
+
+DRAW BARCHART WITH
+  ORIENTATION VERTICAL
+  AXIS RIGHT
+  AXIS TOP;
+
+SELECT
+  city AS series,
+  month AS x,
+  temperature AS y
+FROM
+  city_temperatures
+WHERE
+  city = "London";
