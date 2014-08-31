c3e1c3aea67d98c003dc213d12dbd043a76fddc8
Paul Asmuth <paul@paulasmuth.com>
2014-09-01 00:44:57 +0100

    ...

--- doc/examples/combined/barsandlines.sql
+++ doc/examples/combined/barsandlines.sql
@@ -1,29 +1,19 @@
 IMPORT TABLE city_temperatures
-   FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
+    FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
 
 DRAW BARCHART WITH
-  ORIENTATION VERTICAL
-  AXIS RIGHT
-  AXIS TOP;
+    ORIENTATION VERTICAL
+    AXIS RIGHT
+    AXIS TOP;
 
-SELECT
-  city AS series,
-  month AS x,
-  temperature AS y
-FROM
-  city_temperatures
-WHERE
-  city = "London";
+SELECT city AS series, month AS x, temperature AS y
+    FROM city_temperatures
+    WHERE city = "London";
 
 DRAW LINECHART WITH
-  AXIS BOTTOM
-  AXIS LEFT;
+    AXIS BOTTOM
+    AXIS LEFT;
 
-SELECT
-  city AS series,
-  month AS x,
-  temperature AS y
-FROM
-  city_temperatures
-WHERE
-  city = "Tokyo";
+SELECT city AS series, month AS x, temperature AS y, "circle" as pointstyle, "color2" as color
+    FROM city_temperatures
+    WHERE city = "Tokyo";
