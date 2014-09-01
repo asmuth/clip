104f5f279ecc230be77090229ff1abbc689d5c99
Paul Asmuth <paul@paulasmuth.com>
2014-09-02 00:13:35 +0100

    full new homepage

--- doc/examples/barcharts/vertical_bars.sql
+++ doc/examples/barcharts/vertical_bars.sql
@@ -1,15 +1,10 @@
 IMPORT TABLE city_temperatures
    FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
 
-DRAW BARCHART WITH
-  ORIENTATION VERTICAL
-  AXIS BOTTOM;
+DRAW LINE WITH
+   AXIS BOTTOM;
 
-SELECT
-  city AS series,
-  month AS x,
-  temperature AS y
-FROM
-  city_temperatures;
+SELECT city AS series, month AS x, temperature AS y
+   FROM city_temperatures;
 
 
