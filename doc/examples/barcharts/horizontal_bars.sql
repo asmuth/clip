4e90b1c5f40433e3762ac8f2a3fefecbe33fa0c9
Paul Asmuth <paul@paulasmuth.com>
2014-09-07 00:41:03 +0100

    examples...

--- doc/examples/barcharts/horizontal_bars.sql
+++ doc/examples/barcharts/horizontal_bars.sql
@@ -1,10 +1,11 @@
-IMPORT TABLE gdp_per_country
-   FROM CSV 'test/fixtures/gbp_per_country_simple.csv' HEADER;
+IMPORT TABLE gdp_per_capita
+   FROM CSV 'doc/examples/data/gdp_per_capita.csv' HEADER;
 
 DRAW BARCHART WITH
     ORIENTATION HORIZONTAL
     AXIS LEFT
     AXIS BOTTOM;
 
-SELECT 'gdp per country' AS series, country AS x, gbp AS y, country as label
-    FROM gdp_per_country LIMIT 10;
+SELECT 'gdp per country' AS series, isocode AS x, gdp AS y
+    FROM gdp_per_capita
+    LIMIT 100 OFFSET 200;
--- doc/examples/linecharts/simple_lines.sql
+++ doc/examples/linecharts/simple_lines.sql
@@ -1,14 +1,10 @@
-IMPORT TABLE city_temperatures
-   FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
+IMPORT TABLE example_data
+   FROM CSV 'doc/examples/data/measurement.csv' HEADER;
 
 DRAW LINECHART WITH
   AXIS BOTTOM
   AXIS LEFT;
 
-SELECT
-  city AS series,
-  month AS x,
-  temperature AS y,
-  1 as linewidth
-FROM
-  city_temperatures;
+SELECT 'data' AS series, (time - 1404278100) / 60 AS x, value2 * 1000 AS y
+   FROM example_data
+   WHERE series = "measurement2";
