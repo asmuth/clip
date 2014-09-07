271b0088b8aee74d9a9580ee99e88b9d4efb934a
Paul Asmuth <paul@paulasmuth.com>
2014-09-07 20:47:52 +0100

    wire up legend positions

--- doc/examples/linecharts/lines_with_points.sql
+++ doc/examples/linecharts/lines_with_points.sql
@@ -2,11 +2,9 @@ IMPORT TABLE city_temperatures
    FROM CSV 'doc/examples/data/city_temperatures.csv' HEADER;
 
 DRAW LINECHART WITH
-   AXIS BOTTOM TITLE "fnord"
-   AXIS LEFT TITLE "fnord"
-   AXIS TOP TITLE "fnord"
-   AXIS RIGHT TITLE "fnord"
-   LEGEND TOP RIGHT INSIDE;
+   AXIS LEFT
+   AXIS BOTTOM
+   LEGEND TOP LEFT INSIDE;
 
 SELECT city AS series, month AS x, temperature AS y, "circle" as pointstyle
    FROM city_temperatures;
