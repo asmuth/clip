4e59bcf4720ccdb37c6527d28f8869fa1b9fd40a
Laura Schlimmer <laura@etat-gray.com>
2014-11-05 23:33:19 +0000

    enable autocomplete input

--- fnordmetric-webui/fnordmetric-webui.js
+++ fnordmetric-webui/fnordmetric-webui.js
@@ -899,10 +899,9 @@ FnordMetric.views.QueryPlayground = function() {
           parentNode.appendChild(title);
         }
 
-        function createField(title_text) {
+        function createField() {
           var field = document.createElement("div");
           field.className = "visual_editor field";
-          renderFieldTitle(title_text, field);
           return field;
         }
 
@@ -917,7 +916,7 @@ FnordMetric.views.QueryPlayground = function() {
           return dropdown;
         }
 
-        var metric_field = createField("Metric");
+        var metric_field = createField();
         var metric = document.createElement("input");
         var metric_list = document.createElement("ul");
         metric_list.className = "dropdown";
@@ -927,12 +926,13 @@ FnordMetric.views.QueryPlayground = function() {
         });
         console.log(keys);
         metric_field.appendChild(metric);
-        metric_field.appendChild(metric_list);
+        //metric_field.appendChild(metric_list);
 
         FnordMetric.DropdownAutocomplete(
           metric_field, metric_list, metric, keys, undefined);
 
-        var draw_field = createField("DRAW");
+        var draw_field = createField();
+        renderFieldTitle("DRAW", draw_field);
         var draw_dropdown = createDropdown(["LINECHART", 
           "POINTCHART","BARCHART", "AREACHART"], false);
         draw_field.appendChild(draw_dropdown);
