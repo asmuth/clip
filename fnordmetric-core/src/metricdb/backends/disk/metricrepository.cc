d24b561c1d078f0def680efec68b6a5b35d55c94
Paul Asmuth <paul@paulasmuth.com>
2014-11-09 01:28:36 +0000

    disk_backend::Metric

--- fnordmetric-core/src/metricdb/backends/disk/metricrepository.cc
+++ fnordmetric-core/src/metricdb/backends/disk/metricrepository.cc
@@ -17,10 +17,28 @@ namespace disk_backend {
 MetricRepository::MetricRepository(
     const std::string data_dir) :
     file_repo_(new fnord::io::FileRepository(data_dir)) {
+  std::unordered_map<
+      std::string,
+      std::vector<std::unique_ptr<TableRef>>> tables;
+
+  file_repo_->listFiles([this, &tables] (const std::string& filename) -> bool {
+    auto table_ref = TableRef::openTable(filename);
+    tables[table_ref->metricKey()].emplace_back(std::move(table_ref));
+    return true;
+  });
+
+  for (auto& iter : tables) {
+    auto metric = new Metric(
+        iter.first,
+        file_repo_.get(),
+        std::move(iter.second));
+
+    metrics_.emplace(iter.first, std::unique_ptr<Metric>(metric));
+  }
 }
 
 Metric* MetricRepository::createMetric(const std::string& key) {
-  RAISE(kNotImplementedError, "NYI");
+  return new Metric(key, file_repo_.get());
 }
 
 }
