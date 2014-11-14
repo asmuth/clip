474bb3ef27e7c6f98e274086cf3cc5bbccbd5841
Paul Asmuth <paul@paulasmuth.com>
2014-11-14 20:18:18 +0000

    bring back FNV test, delete dead code

--- fnordmetric-core/src/util/fnv_test.cc
+++ fnordmetric-core/src/util/fnv_test.cc
@@ -1,27 +1,26 @@
 /**
  * This file is part of the "FnordMetric" project
- *   Copyright (c) 2011-2014 Paul Asmuth, Google Inc.
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
  *
  * FnordMetric is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License v3.0. You should have received a
  * copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include "fnv.h"
+#include <fnordmetric/util/fnv.h>
+#include <fnordmetric/util/unittest.h>
 
-void testSimpleHashing() {
-  fnordmetric::FNV<uint64_t> fnv64;
-  fnordmetric::FNV<uint32_t> fnv32;
+UNIT_TEST(FNVTest);
 
-  assert(fnv64.hash("fnord") == 0xE4D8CB6A3646310);
-  assert(fnv32.hash("fnord") == 0x6D964EB0);
-}
+TEST_CASE(FNVTest, TestFNV64, [] () {
+  fnord::util::FNV<uint64_t> fnv64;
+  EXPECT_EQ(fnv64.hash("fnord"), 0xE4D8CB6A3646310);
+});
 
-
-int main() {
-  testSimpleHashing();
-}
+TEST_CASE(FNVTest, TestFNV32, [] () {
+  fnord::util::FNV<uint32_t> fnv32;
+  EXPECT_EQ(fnv32.hash("fnord"), 0x6D964EB0);
+});
