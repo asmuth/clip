a63704403139af03901566f6525af32accfd85c6
Paul Asmuth <paul@paulasmuth.com>
2014-11-20 20:34:32 +0000

    simple paginated table

---
+++ libfnord/webui/fnord.js
@@ -0,0 +1,31 @@
+/**
+ * This file is part of the "FnordMetric" project
+ *   Copyright (c) 2014 Laura Schlimmer
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+Fnord = {
+  components: {}
+};
+
+Fnord.getTemplate = function(component, template) {
+  var i = document.querySelector("link[data-component='" + component + "']");
+
+  if (!i) {
+    return null;
+  }
+
+  return i.import.querySelector("#" + component + "-" + template + "-tpl");
+}
+
+Fnord.registerComponent = function(component, cb) {
+  var proto = Object.create(HTMLElement.prototype);
+  cb.apply(proto);
+  Fnord.components[component] = proto;
+  document.registerElement(component, { prototype: proto });
+  return proto;
+}
