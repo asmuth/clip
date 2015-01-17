0ee7b054f962a9070a11f3f30ef162fb659d5cf0
Paul Asmuth <paul@paulasmuth.com>
2015-01-17 23:42:55 +0000

    HTTPRPCClient stub

--- fnord-rpc/RPCClient.cc
+++ fnord-rpc/RPCClient.cc
@@ -7,6 +7,7 @@
  * copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
+#include "fnord/base/logging.h"
 #include "fnord-rpc/RPCClient.h"
 
 namespace fnord {
@@ -16,7 +17,46 @@ HTTPRPCClient::HTTPRPCClient(
     http_pool_(sched) {}
 
 void HTTPRPCClient::call(const URI& uri, RefPtr<AnyRPC> rpc) {
+  http::HTTPRequest http_req(http::HTTPRequest::M_POST, uri.path());
 
+  auto rpc_serialized = "fnord";
+  http_req.addBody(rpc_serialized);
+  http_req.setHeader("Host", uri.hostAndPort());
+
+  auto http_future = http_pool_.executeRequest(http_req);
+
+#ifndef FNORD_NOTRACE
+  fnord::logTrace(
+      "fnord.http.rpcclient",
+      "executing RPC via HTTP request id=$2 method=$1 uri=$0",
+      uri.toString(),
+      rpc->method(),
+      (void*) rpc.get());
+#endif
+
+  http_future.onSuccess([uri, rpc] (const http::HTTPResponse& resp) {
+#ifndef FNORD_NOTRACE
+    fnord::logTrace(
+        "fnord.http.rpcclient",
+        "RPC via HTTP request id=$0: Success, got $1 bytes response",
+        (void*) rpc.get(),
+        123);
+#endif
+    fnord::iputs("got response!!", 1);
+  });
+
+  http_future.onFailure([uri, rpc] (const Status& status) mutable{
+#ifndef FNORD_NOTRACE
+    fnord::logTrace(
+        "fnord.http.rpcclient",
+        "RPC via HTTP request id=$0: Failed: $1",
+        (void*) rpc.get(),
+        status);
+#endif
+
+    fnord::iputs("got error!!", 1);
+    rpc->error(status);
+  });
 }
 
 } // namespace fnord
