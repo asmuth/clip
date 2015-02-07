b982e430cdc42a5839a8fe6338620f84469d5258
Laura Schlimmer <laura@etat-gray.com>
2015-02-07 23:05:10 +0000

    metric-control component

--- src/fnordmetric/adminui.cc
+++ src/fnordmetric/adminui.cc
@@ -55,6 +55,7 @@ AdminUI::AdminUI(
   webui_bundle_.addComponent("fnord-metricdb/metric-explorer-list.html");
   webui_bundle_.addComponent("fnord-metricdb/metric-explorer-preview.html");
   webui_bundle_.addComponent("fnord-metricdb/metric-explorer-search.html");
+  webui_bundle_.addComponent("fnord-metricdb/metric-control.html");
   webui_bundle_.addComponent("fnord-metricdb/fn-metric-explorer.css");
   webui_bundle_.addComponent("fnordmetric/fnordmetric-app.html");
   webui_bundle_.addComponent("fnordmetric/fnordmetric-console.html");
