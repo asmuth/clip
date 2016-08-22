d9fdfad9e92e3029e29ceda4e483f4fc12c72ec2
Paul Asmuth <paul@paulasmuth.com>
2016-08-22 16:13:41 +0200

    cleaning up

--- src/fnordmetric/util/inspect.cc
+++ src/fnordmetric/util/inspect.cc
@@ -30,8 +30,12 @@ std::string inspect<unsigned long>(const unsigned long& value) {
 }
 
 template <>
-std::string inspect<unsigned long long>(
-    const unsigned long long& value) {
+std::string inspect<unsigned long long>(const unsigned long long& value) {
+  return std::to_string(value);
+}
+
+template <>
+std::string inspect<unsigned char>(const unsigned char& value) {
   return std::to_string(value);
 }
 
@@ -44,6 +48,12 @@ std::string inspect<long long>(
 template <>
 std::string inspect<long>(
     const long& value) {
+  return std::to_string(value);
+}
+
+template <>
+std::string inspect<unsigned short>(
+    const unsigned short& value) {
   return std::to_string(value);
 }
 
@@ -63,6 +73,13 @@ std::string inspect<std::string>(const std::string& value) {
 }
 
 template <>
+std::string inspect<std::wstring>(const std::wstring& value) {
+  std::string out;
+  out.assign(value.begin(), value.end());
+  return out;
+}
+
+template <>
 std::string inspect<char const*>(char const* const& value) {
   return std::string(value);
 }
@@ -75,5 +92,10 @@ std::string inspect<void*>(void* const& value) {
 template <>
 std::string inspect<const void*>(void const* const& value) {
   return "<ptr>";
+}
+
+template <>
+std::string inspect<std::exception>(const std::exception& e) {
+  return e.what();
 }
 
