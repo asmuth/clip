ded3e6f5daef33f15ff1aca7787ed9a81a2b9365
Laura Schlimmer <laura@zscale.io>
2016-08-22 16:51:20 +0200

    new frontend

--- fnordmetric-webui/views/metric-list/metric-list.js
+++ fnordmetric-webui/views/metric-list/metric-list.js
@@ -2,13 +2,18 @@ FnordMetric.views["fnordmetric.metric.list"] = function(elem, params) {
   var query_mgr = EventSourceHandler();
 
   this.initialize = function(params) {
-    var page = zTemplateUtil.getTemplate("");
+    var page = zTemplateUtil.getTemplate("fnordmetric.metric.list.tpl");
+    elem.appendChild(page);
 
+    fetchMetricList();
   };
 
   this.destroy = function() {
     query_mgr.closeAll();
   };
 
+  var fetchMetricList = function() {
+
+  };
 
 };
