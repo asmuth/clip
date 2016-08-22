c5beb810d9b55479aaf5cebf8190e6b26b54de38
Laura Schlimmer <laura@zscale.io>
2016-08-22 19:24:47 +0200

    render metric list view stub

--- src/fnordmetric/webui/views/metric-list/metric-list.js
+++ src/fnordmetric/webui/views/metric-list/metric-list.js
@@ -1,19 +1,26 @@
 FnordMetric.views["fnordmetric.metric.list"] = function(elem, params) {
-  var query_mgr = EventSourceHandler();
 
   this.initialize = function(params) {
-    var page = zTemplateUtil.getTemplate("fnordmetric.metric.list.tpl");
+    var page = zTemplateUtil.getTemplate("fnordmetric-metric-list-tpl");
     elem.appendChild(page);
 
-    fetchMetricList();
+    loadMetricList();
   };
 
   this.destroy = function() {
     query_mgr.closeAll();
   };
 
-  var fetchMetricList = function() {
-
+  var loadMetricList = function() {
+    var headers = {};
+    zHTTP.httpGet("/api/metrics", headers, function(r) {
+      if (r.status == 200) {
+        var json = JSON.parse(r.response);
+        renderMetricList(json);
+      } else {
+        FnordMetric.renderError("An error occured while loading the metric list");
+      }
+    });
   };
 
 };
