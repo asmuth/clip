a9acda8c55b7cf93d5b9167965c194306854de91
Laura Schlimmer <laura@zscale.io>
2016-08-23 15:56:48 +0200

    build initial metric detail view config

--- src/fnordmetric/webui/views/metric-table/metric-table.js
+++ src/fnordmetric/webui/views/metric-table/metric-table.js
@@ -1,6 +1,51 @@
 FnordMetric.views["fnordmetric.metric.table"] = function(elem, params) {
+
+  var ViewConfig = function(metric_cfg, uri_params) {
+    var cfg = {
+      id_columns: [],
+      value_columns: []
+    }
+
+    this.update = function(property, value) {
+
+    }
+
+    this.get = function() {
+      return cfg;
+    }
+
+    this.getQueryString = function() {
+
+    }
+
+    var initialize = function() {
+      var max_id_cols = 3;
+      var max_value_cols = 4;
+
+      if (metric_cfg.id_columns) {
+        cfg.id_columns = metric_cfg.id_columns.slice(0, max_id_cols);
+
+        // display more value columns if less than max id columns
+        max_value_cols += max_id_cols - cfg.id_columns.length;
+      }
+
+      if (metric_cfg.id_columns) {
+        cfg.value_columns = metric_cfg.value_columns.slice(0, max_value_cols);
+      }
+
+      cfg.offset = 0;
+      cfg.limit = 25;
+      cfg.order = cfg.value_columns[0].id;
+    }
+
+    initialize();
+  };
+
   var url_params = {};
   var table = new zTable({});
+
+  //info about the current view
+  var view_cfg = {};
 
   this.initialize = function() {
     url_params = getUrlParams(params.path);
@@ -10,12 +55,11 @@ FnordMetric.views["fnordmetric.metric.table"] = function(elem, params) {
     zDomUtil.handleLinks(page, params.app.navigateTo);
     elem.appendChild(page);
 
-
-    renderTable();
     table.onClick(function(r) {
       params.app.navigateTo("/metrics/" + r.metric_id);
     });
 
+    loadMetricConfig();
   };
 
   this.destroy = function() {
@@ -33,9 +77,39 @@ FnordMetric.views["fnordmetric.metric.table"] = function(elem, params) {
     return p;
   };
 
+  var loadMetricConfig = function() {
+    //REMOVE ME
+    var metric_cfg = {
+      id_columns: [
+        { "id": "host" },
+        { "id": "datacenter" },
+      ],
+      value_columns: [
+        { "id": "cpu_time" },
+        { "id": "mem_used" },
+        { "id": "mem_free" },
+        { "id": "uptime" },
+        { "id": "load_avg" }
+      ]
+    };
+    //REMOVE ME END
+
+    view_cfg = new ViewConfig(metric_cfg, url_params);
+    renderTable();
+  };
+
+  var updateViewConfig = function() {
+
+  }
+
   var renderHeader = function(metric) {
     var header = elem.querySelector(
         ".fnordmetric-metric-table .page_header .metric_name");
     header.innerHTML = metric;
+  };
+
+  var renderTable = function() {
+    elem.querySelector(".fnordmetric-metric-table .metric-table")
+        .innerHTML = JSON.stringify(view_cfg.get());
   };
 }
