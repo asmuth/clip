51da86ad629d7e4a212cb71a6fe34b5b33f1df2f
Laura Schlimmer <laura@zscale.io>
2016-09-01 11:36:42 +0200

    metric-detail: render timeseries with DyGraph

--- src/fnordmetric/webui/views/metric-detail/metric-timeseries.js
+++ src/fnordmetric/webui/views/metric-detail/metric-timeseries.js
@@ -2,13 +2,73 @@ FnordMetric.views["fnordmetric.metric.timeseries"] = function(elem, params) {
   var table;
 
   this.initialize = function() {
-    //var page = zTemplateUtil.getTemplate("fnordmetric-metric-timeseries-tpl");
+    var page = zTemplateUtil.getTemplate("fnordmetric-metric-timeseries-tpl");
+    elem.appendChild(page);
 
-    //zDomUtil.handleLinks(page, params.app.navigateTo);
-    //elem.appendChild(page);
-
-    console.log("timeseries view");
+    renderCharts(params.data);
   };
+
+  var renderCharts = function(results) {
+    var chart_container = elem.querySelector(".timeseries");
+    var chart_tpl = zTemplateUtil.getTemplate(
+        "fnordmetric-metric-timeseries-chart-tpl").querySelector(".chart_box");
+
+    var chart_boxes = [];
+    results.forEach(function(r) {
+      var chart_box = chart_tpl.cloneNode(true);
+      chart_container.appendChild(chart_box);
+      chart_boxes.push(chart_box);
+    });
+
+    console.log(results.length);
+
+    for (var j = 0; j < results.length; j++) {
+      var result = results[j];
+      var time_col_idx;
+      for (var i = 0; i < result.columns.length; i++) {
+        if (result.columns[i] == "time") {
+          time_col_idx = i;
+          break;
+        }
+      }
+
+      if (typeof time_col_idx !== "number") {
+        console.log("ERROR: no time column", result.columns);
+        return;
+      }
+
+      var rows = [];
+      result.rows.forEach(function(r) {
+        var row = [];
+        for (var i = 0; i < r.length; i++) {
+          if (i == time_col_idx) {
+            row.push(new Date(r[i]));
+          } else {
+            row.push(r[i]);
+
+            var test = r[i] + i * 100
+            row.push(test);
+          }
+        }
+
+        rows.push(row);
+      });
+
+      var columns = result.columns;
+      columns.push("test");
+
+
+      new Dygraph(
+          chart_boxes[j].querySelector(".chart_canvas"),
+          rows,
+          {
+            labels: result.columns,
+            title: result.title,
+            colors: ["#3491DF", "#99C8E2"],
+          });
+    }
+
+  }
   
 }
 
