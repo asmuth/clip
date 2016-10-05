9bdf10093f5730fb0ef5b97b1ed73a3eefb4eeb1
Paul Asmuth <paul@paulasmuth.com>
2016-10-05 22:42:14 +0200

    improved unit test

--- libtsdb/test/smoketest.cc
+++ libtsdb/test/smoketest.cc
@@ -85,15 +85,11 @@ TEST_CASE(TSDBTest, TestCreateAndInsert, [] () {
     std::unique_ptr<tsdb::TSDB> db;
     EXPECT(tsdb::TSDB::openDatabase(&db, "/tmp/__test.tsdb") == true);
 
-    for (size_t i = 200000; i < 300000; ++i) {
-      EXPECT(db->insertUInt64(1, t0 + 20 * i, i) == true);
-    }
-
     {
       tsdb::Cursor cursor(tsdb::PageType::UINT64);
       EXPECT(db->getCursor(1, &cursor) == true);
 
-      for (size_t i = 0; i < 300000; ++i) {
+      for (size_t i = 0; i < 200000; ++i) {
         uint64_t ts;
         uint64_t value;
         EXPECT(cursor.next(&ts, &value) == true);
@@ -102,13 +98,25 @@ TEST_CASE(TSDBTest, TestCreateAndInsert, [] () {
       }
     }
 
+    for (size_t i = 300000; i < 400000; ++i) {
+      EXPECT(db->insertUInt64(1, t0 + 20 * i, i) == true);
+    }
+
     EXPECT(db->commit() == true);
 
     {
       tsdb::Cursor cursor(tsdb::PageType::UINT64);
       EXPECT(db->getCursor(1, &cursor) == true);
 
-      for (size_t i = 0; i < 300000; ++i) {
+      for (size_t i = 0; i < 200000; ++i) {
+        uint64_t ts;
+        uint64_t value;
+        EXPECT(cursor.next(&ts, &value) == true);
+        EXPECT(ts == t0 + 20 * i);
+        EXPECT(value == i);
+      }
+
+      for (size_t i = 300000; i < 400000; ++i) {
         uint64_t ts;
         uint64_t value;
         EXPECT(cursor.next(&ts, &value) == true);
@@ -122,21 +130,21 @@ TEST_CASE(TSDBTest, TestCreateAndInsert, [] () {
     std::unique_ptr<tsdb::TSDB> db;
     EXPECT(tsdb::TSDB::openDatabase(&db, "/tmp/__test.tsdb") == true);
 
+    for (size_t i = 200000; i < 300000; ++i) {
+      EXPECT(db->insertUInt64(1, t0 + 20 * i, i) == true);
+    }
+
     {
       tsdb::Cursor cursor(tsdb::PageType::UINT64);
       EXPECT(db->getCursor(1, &cursor) == true);
 
-      for (size_t i = 0; i < 300000; ++i) {
+      for (size_t i = 0; i < 400000; ++i) {
         uint64_t ts;
         uint64_t value;
         EXPECT(cursor.next(&ts, &value) == true);
         EXPECT(ts == t0 + 20 * i);
         EXPECT(value == i);
       }
-    }
-
-    for (size_t i = 300000; i < 400000; ++i) {
-      EXPECT(db->insertUInt64(1, t0 + 20 * i, i) == true);
     }
 
     EXPECT(db->commit() == true);
