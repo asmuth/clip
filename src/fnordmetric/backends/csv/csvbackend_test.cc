5b2cc134f0c47981e19bbd2d0555d4c9be07a691
Paul Asmuth <paul@asmuth.com>
2017-03-03 20:54:06 +0100

    src/fnordmetric/backends from master

---
+++ src/fnordmetric/backends/csv/csvbackend_test.cc
@@ -0,0 +1,150 @@
+/**
+ * This file is part of the "FnordStream" project
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * Licensed under the MIT license (see LICENSE).
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fnordmetric/util/runtimeexception.h>
+#include <fnordmetric/util/unittest.h>
+#include <fnordmetric/sql/backends/csv/csvinputstream.h>
+#include <fnordmetric/sql/backends/csv/csvtableref.h>
+#include <fnordmetric/sql/svalue.h>
+
+using namespace fnordmetric::query::csv_backend;
+
+UNIT_TEST(CSVInputStreamTest);
+
+TEST_CASE(CSVInputStreamTest, TestOpenFile, [] () {
+  auto csv_file = CSVInputStream::openFile(
+      "test/fixtures/gbp_per_country.csv");
+
+  EXPECT(csv_file.get() != nullptr);
+});
+
+TEST_CASE(CSVInputStreamTest, TestInvalidFileName, [] () {
+  auto errmsg = "error opening file 'test/fixtures/invalid.csv': "
+      "No such file or directory";
+
+  EXPECT_EXCEPTION(errmsg, [] () {
+    auto csv_file = CSVInputStream::openFile("test/fixtures/invalid.csv");
+  });
+});
+
+TEST_CASE(CSVInputStreamTest, TestReadHeaders, [] () {
+  auto csv_file = CSVInputStream::openFile(
+      "test/fixtures/gbp_per_country_simple.csv");
+
+  EXPECT(csv_file.get() != nullptr);
+  std::vector<std::string> headers;
+  csv_file->readNextRow(&headers);
+  EXPECT_EQ(headers.size(), 2);
+  EXPECT_EQ(headers[0], "country");
+  EXPECT_EQ(headers[1], "gbp");
+});
+
+TEST_CASE(CSVInputStreamTest, TestReadSimpleRows, [] () {
+  auto csv_file = CSVInputStream::openFile(
+      "test/fixtures/gbp_per_country_simple.csv");
+  EXPECT(csv_file.get() != nullptr);
+
+  std::vector<std::string> headers;
+  csv_file->readNextRow(&headers);
+  EXPECT_EQ(headers.size(), 2);
+
+  std::vector<std::string> row1;
+  EXPECT(csv_file->readNextRow(&row1));
+  EXPECT_EQ(row1.size(), 2);
+  EXPECT_EQ(row1[0], "USA");
+  EXPECT_EQ(row1[1], "16800000");
+
+  std::vector<std::string> row2;
+  EXPECT(csv_file->readNextRow(&row2));
+  EXPECT_EQ(row2.size(), 2);
+  EXPECT_EQ(row2[0], "CHN");
+  EXPECT_EQ(row2[1], "9240270");
+});
+
+TEST_CASE(CSVInputStreamTest, TestReadSimpleRowsEOF, [] () {
+  auto csv_file = CSVInputStream::openFile(
+      "test/fixtures/gbp_per_country_simple.csv");
+  EXPECT(csv_file.get() != nullptr);
+  int num_rows = 0;
+
+  for (;; ++num_rows) {
+    std::vector<std::string> row;
+
+    if (!csv_file->readNextRow(&row)) {
+      break;
+    }
+
+    EXPECT_EQ(row.size(), 2);
+  }
+
+  EXPECT_EQ(num_rows, 192);
+});
+
+// CSVTableRefTest
+TEST_CASE(CSVInputStreamTest, TestGetColumnIndexWithHeaders, [] () {
+  CSVTableRef table_ref(
+      CSVInputStream::openFile("test/fixtures/gbp_per_country_simple.csv"),
+      true);
+
+  EXPECT_EQ(table_ref.getColumnIndex("country"), 0);
+});
+
+// CSVTableRefTest
+TEST_CASE(CSVInputStreamTest, TestReadRowsWithHeaders, [] () {
+  CSVTableRef table_ref(
+      CSVInputStream::openFile("test/fixtures/gbp_per_country_simple.csv"),
+      true);
+
+  for (int n = 0; n < 2; n++) {
+    int num_rows = 0;
+    for (;; ++num_rows) {
+      std::vector<fnordmetric::query::SValue> target;
+      if (!table_ref.readNextRow(&target)) {
+        break;
+      }
+    }
+
+    EXPECT_EQ(num_rows, 191);
+    table_ref.rewind();
+  }
+});
+
+// CSVTableRefTest
+TEST_CASE(CSVInputStreamTest, TestGetColumnIndexWithoutHeaders, [] () {
+  CSVTableRef table_ref(
+      CSVInputStream::openFile(
+          "test/fixtures/gbp_per_country_simple_noheaders.csv"),
+      false);
+
+  EXPECT_EQ(table_ref.getColumnIndex("col1"), 0);
+  EXPECT_EQ(table_ref.getColumnIndex("col2"), 1);
+  EXPECT_EQ(table_ref.getColumnIndex("col3"), 2);
+  EXPECT_EQ(table_ref.getColumnIndex("col99"), 98);
+});
+
+// CSVTableRefTest
+TEST_CASE(CSVInputStreamTest, TestReadRowsWithoutHeaders, [] () {
+  CSVTableRef table_ref(
+      CSVInputStream::openFile(
+          "test/fixtures/gbp_per_country_simple_noheaders.csv"),
+      false);
+
+  int num_rows = 0;
+  for (;; ++num_rows) {
+    std::vector<fnordmetric::query::SValue> target;
+    if (!table_ref.readNextRow(&target)) {
+      break;
+    }
+  }
+
+  EXPECT_EQ(num_rows, 191);
+});
+
+
+
---
+++ src/fnordmetric/backends/postgres/postgresbackend.h
@@ -0,0 +1,42 @@
+/**
+ * This file is part of the "FnordMetric" project
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FNORDMETRIC_POSTGRESBACKEND_H
+#define _FNORDMETRIC_POSTGRESBACKEND_H
+#include <fnordmetric/sql/backends/backend.h>
+#include <fnordmetric/sql/backends/postgres/postgresconnection.h>
+#include <memory>
+#include <mutex>
+#include <vector>
+
+namespace fnordmetric {
+namespace query {
+namespace postgres_backend {
+
+class PostgresBackend : public fnordmetric::query::Backend {
+public:
+
+  static PostgresBackend* singleton();
+
+  PostgresBackend();
+
+  bool openTables(
+      const std::vector<std::string>& table_names,
+      const util::URI& source_uri,
+      std::vector<std::unique_ptr<TableRef>>* target) override;
+
+protected:
+  std::vector<std::shared_ptr<PostgresConnection>> connections_;
+  std::mutex connections_mutex_;
+};
+
+}
+}
+}
+#endif
