0e68a3948b754e125f04ee56704d758a0b303620
Paul Asmuth <paul@asmuth.com>
2017-05-04 19:16:03 +0200

    add empty js/python/ruby/rust bindings

---
+++ bindings/python/README.md
@@ -0,0 +1,1 @@
+# metrictools python binding
---
+++ bindings/python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'metrictools',
+  packages = ['metrictools'],
+  version = '0.1',
+  description = 'metrictools python binding',
+  author = 'Paul Asmuth',
+  author_email = 'paul@asmuth.com',
+  url = 'https://github.com/paulasmuth/metrictools',
+  download_url = 'https://github.com/paulasmuth/metrictools/tarball/0.1',
+  keywords = ['metrictools']
+)
