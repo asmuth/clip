1a4607e0b3633c25a661e3ecc2d17b816ec83015
Paul Asmuth <paul@asmuth.com>
2018-11-30 15:14:02 +0100

    add layout_margin_box helper

---
+++ common/graphics/layout.cc
@@ -0,0 +1,67 @@
+/**
+ * This file is part of the "plotfx" project
+ *   Copyright (c) 2018 Paul Asmuth
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <algorithm>
+#include "layout.h"
+
+namespace plotfx {
+
+Rectangle::Rectangle() :
+    x(0.0f),
+    y(0.0f),
+    w(0.0f),
+    h(0.0f) {}
+
+Rectangle::Rectangle(
+    double _x,
+    double _y,
+    double _w,
+    double _h) :
+    x(_x),
+    y(_y),
+    w(_w),
+    h(_h) {}
+
+Rectangle layout_margin_box(
+    const Rectangle& parent,
+    double margin_top,
+    double margin_right,
+    double margin_bottom,
+    double margin_left) {
+  Rectangle box;
+  box.x = parent.x + margin_left;
+  box.y = parent.y + margin_top;
+  box.w = std::max(parent.w - (margin_left + margin_right), 0.0);
+  box.h = std::max(parent.h - (margin_top + margin_bottom), 0.0);
+  return box;
+}
+
+
+} // namespace plotfx
+
