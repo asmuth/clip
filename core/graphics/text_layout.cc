b3998725d5165afc8235992de56ab7e864f82546
Paul Asmuth <paul@asmuth.com>
2019-07-26 20:15:27 +0200

    perform text shaping on a per-grapheme basis

--- core/graphics/text_layout.cc
+++ core/graphics/text_layout.cc
@@ -15,8 +15,51 @@
 #include "graphics/text_layout.h"
 #include "graphics/text_shaper.h"
 
+#include <boost/locale.hpp>
+
 namespace fviz {
 namespace text {
+
+struct TextRun {
+  FontInfo font;
+  std::vector<GlyphInfo> glyphs;
+};
+
+Status text_layout_with_font_fallback(
+    const std::string& text,
+    const FontInfo& font_info,
+    double font_size,
+    double dpi,
+    const TextShaper* shaper,
+    std::vector<TextRun>* text_runs) {
+
+  boost::locale::generator locale_gen;
+  boost::locale::boundary::ssegment_index grapheme_iter(
+        boost::locale::boundary::character,
+        text.begin(),
+        text.end(),
+        locale_gen("en_US.UTF-8"));
+
+  for (const auto& grapheme_str : grapheme_iter) {
+    TextRun grapheme;
+    grapheme.font = font_info;
+
+    auto rc = shaper->shapeText(
+        grapheme_str,
+        font_info,
+        font_size,
+        dpi,
+        &grapheme.glyphs);
+
+    if (rc != OK) {
+      return rc;
+    }
+
+    text_runs->emplace_back(grapheme);
+  }
+
+  return OK;
+}
 
 Status text_layout_ltr(
     const std::string& text,
@@ -26,35 +69,39 @@ Status text_layout_ltr(
     const TextShaper* shaper,
     std::vector<GlyphPlacement>* glyphs,
     Rectangle* bbox) {
-  std::vector<GlyphInfo> glyphs_raw;
-  auto rc = shaper->shapeText(
+  std::vector<TextRun> text_runs;
+
+  auto shaping_rc = text_layout_with_font_fallback(
       text,
       font_info,
       font_size,
       dpi,
-      &glyphs_raw);
+      shaper,
+      &text_runs);
 
-  if (rc != OK) {
-    return rc;
+  if (!shaping_rc) {
+    return shaping_rc;
   }
 
   double line_length = 0.0f;
   double line_top = 0.0f;
   double line_bottom = 0.0f;
-  for (const auto& gi : glyphs_raw) {
-    GlyphPlacement gp = {
-      .codepoint = gi.codepoint,
-      .x = line_length,
-      .y = 0
-    };
+  for (const auto& r : text_runs) {
+    for (const auto& gi : r.glyphs) {
+      GlyphPlacement gp = {
+        .codepoint = gi.codepoint,
+        .x = line_length,
+        .y = 0
+      };
 
-    if (glyphs) {
-      glyphs->emplace_back(gp);
+      if (glyphs) {
+        glyphs->emplace_back(gp);
+      }
+
+      line_length += gi.advance_x;
+      line_top = std::min(-gi.metrics_ascender, line_top);
+      line_bottom = std::max(-gi.metrics_descender, line_bottom);
     }
-
-    line_length += gi.advance_x;
-    line_top = std::min(-gi.metrics_ascender, line_top);
-    line_bottom = std::max(-gi.metrics_descender, line_bottom);
   }
 
   *bbox = Rectangle(
