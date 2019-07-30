efdd5c35730a1db89583627279e2580c174df28b
Paul Asmuth <paul@asmuth.com>
2019-07-30 16:37:03 +0200

    separate example and test execution

---
+++ examples/update_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ue
+cd "$(dirname "$0")/.."
+
+rm tests/spec/examples_*.fvz
+
+find examples -name "*.fvz" | while read file; do
+  cp \
+    "${file}" \
+    "tests/spec/$(echo "${file}" | sed -e 's/^.\///' -e 's/-/_/g' -e 's/\//_/g')"
+done
